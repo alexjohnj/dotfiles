93220155b5e2d7623f30e521118a71291a8589b0
Alex Jackson <alex@alexj.org>
2014-07-31 13:07:59 +0100

    Bootstrap now installs oh-my-fish if it isn't installed

--- bootstrap.sh
+++ bootstrap.sh
@@ -34,18 +34,32 @@ link_file() {
 	ln -s "$src" "$dst"
 }
 
-# Install fish dotfiles
-fish_src="$DOTFILES_ROOT/fish/config.fish"
-fish_dst="$HOME/.config/fish/config.fish"
+install_fish() {
+	# Install oh-my-fish if it isn't installed
 
-info "Installing fish config"
-link_file "$fish_src" "$fish_dst"
-success "Linked fish config"
+	if [ ! -d "$HOME/.oh-my-fish" ]
+	then
+		info "Installing oh-my-fish"
+		curl -L https://github.com/bpinto/oh-my-fish/raw/master/tools/install.fish | fish
+		success "Installing oh-my-fish"
+	fi
 
-# Install fish functions folder
-fish_func_src="$DOTFILES_ROOT/fish/functions"
-fish_func_dst="$HOME/.config/fish/functions"
+	# Install fish dotfiles
+	fish_src="$DOTFILES_ROOT/fish/config.fish"
+	fish_dst="$HOME/.config/fish/config.fish"
 
-info "Installing fish functions"
-link_file "$fish_func_src" "$fish_func_dst"
-success "Installing fish functions"
+	info "Installing fish config"
+	link_file "$fish_src" "$fish_dst"
+	success "Linked fish config"
+
+	# Install fish functions folder
+	fish_func_src="$DOTFILES_ROOT/fish/functions"
+	fish_func_dst="$HOME/.config/fish/functions"
+
+	info "Installing fish functions"
+	link_file "$fish_func_src" "$fish_func_dst"
+	success "Installing fish functions"
+}
+
+install_fish
+
