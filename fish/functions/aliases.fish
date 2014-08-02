1e178d9be78ff0e7677f287de41a7571f4a59502
Alex Jackson <alex@alexj.org>
2014-08-02 09:43:19 +0100

    Removed old project shortcuts

--- fish/functions/aliases.fish
+++ fish/functions/aliases.fish
@@ -2,8 +2,6 @@
 alias dropdev "cd ~/Dropbox/Documents/Development/"
 alias alexjohnj "cd ~/Dropbox/Documents/Development/Web/alexjohnj/"
 alias geographyas "cd ~/Dropbox/Documents/Development/Web/geographyas/"
-alias report-project "cd /Users/alex/Dropbox/Documents/Uni/Fundamentals\ of\ Geophysics/Tutorials/Seismic\ Refraction\ Report"
-alias uni-work "cd /Users/alex/Dropbox/Documents/Uni"
 
 # Copy files with progress information
 alias cpv "rsync -avP"
