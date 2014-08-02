787508e299db3edf26af21a6253774c02c409145
Alex Jackson <alex@alexj.org>
2014-08-02 09:42:21 +0100

    Added cpv alias for fish shell

--- fish/functions/aliases.fish
+++ fish/functions/aliases.fish
@@ -1,5 +1,10 @@
+# cd Shortcuts
 alias dropdev "cd ~/Dropbox/Documents/Development/"
 alias alexjohnj "cd ~/Dropbox/Documents/Development/Web/alexjohnj/"
 alias geographyas "cd ~/Dropbox/Documents/Development/Web/geographyas/"
 alias report-project "cd /Users/alex/Dropbox/Documents/Uni/Fundamentals\ of\ Geophysics/Tutorials/Seismic\ Refraction\ Report"
 alias uni-work "cd /Users/alex/Dropbox/Documents/Uni"
+
+# Copy files with progress information
+alias cpv "rsync -avP"
+
