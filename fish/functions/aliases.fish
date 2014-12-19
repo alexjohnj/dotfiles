f65c8705dadedcca2027a701573c394f03ee06a7
Alex Jackson <alex@alexj.org>
2014-12-19 08:23:45 +0000

    Added ls alias

--- fish/functions/aliases.fish
+++ fish/functions/aliases.fish
@@ -3,6 +3,9 @@ alias dropdev "cd ~/Dropbox/Documents/Development/"
 alias alexjohnj "cd ~/Dropbox/Documents/Development/Web/alexjohnj/"
 alias geographyas "cd ~/Dropbox/Documents/Development/Web/geographyas/"
 
+# ls Aliases
+alias ls "ls -GF"
+
 # Copy files with progress information
 alias cpv "rsync -avP"
 
