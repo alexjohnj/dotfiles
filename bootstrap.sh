7d1bd7cf92437e429d057ae7438dfd9d31c3b675
Alex Jackson <alex@alexj.org>
2014-12-19 20:24:33 +0000

    Attempted to improve bootstrap script on non-OSX platforms

--- bootstrap.sh
+++ bootstrap.sh
@@ -43,57 +43,58 @@ link_file() {
 }
 
 install_homebrew() {
-	if [ "$(uname -s)" == "Darwin" ] && test ! $(which brew)
-	then
-		info "Installing Homebrew"
-		ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-		success "Installing Homebrew"
+	info "Installing Homebrew"
+	if [ $(uname -s) = "Darwin" ];then
+		if ! type "brew" > /dev/null 2>&1;then
+			ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+			success "Installing Homebrew"
+		else
+			success "Installing Homebrew (already installed)"
+		fi
 	else
-		success "Homebrew already installed"
+		info "Not Installing Homebrew"
 	fi
 }
 
 install_fish() {
 	# Install fish if it isn't already
-	if test ! $(which fish)
-	then
-		if [ "$(uname -s)" == "Darwin" ]
-		then
-			info "Installing fish shell"
+	info "Installing fish"
+	if ! type "fish" > /dev/null 2>&1;then
+		if [ $(uname -s) = "Darwin" ] && type "brew" > /dev/null 2>&1;then
 			brew install fish
-			success "Installing fish shell"
+			success "Installing fish"
+		elif [ $(uname -s) = "Linux" ] && type "pacman" > /dev/null 2>&1;then
+			sudo pacman -S fish
+			success "Installing fish"
+		elif [ $(uname -s) = "Linux" ] && type "apt-get" > /dev/null 2>&1;then
+			sudo apt-get install fish
+			success "Installing fish"
 		else
-			fail "Installing fish shell: Install manually"
+			fail "Installing fish (install manually)"
 		fi
 	else
-		success "Fish shell already installed"
+		success "Installing fish (already installed)"
 	fi
 
 	# Install oh-my-fish if it isn't installed
-	if [ ! -d "$HOME/.oh-my-fish" ]
-	then
-		info "Installing oh-my-fish"
+	info "Installing oh-my-fish"
+	if ! [ -d "$HOME/.oh-my-fish" ];then
 		curl -L https://github.com/bpinto/oh-my-fish/raw/master/tools/install.fish | fish
 		success "Installing oh-my-fish"
 	else
-		success "oh-my-fish already installed"
+		success "Installing oh-my-fish (already installed)"
 	fi
 
 	# Install fish dotfiles
 	fish_src="$DOTFILES_ROOT/fish/config.fish"
 	fish_dst="$HOME/.config/fish/config.fish"
-
-	info "Installing fish config"
-	link_file "$fish_src" "$fish_dst"
-	success "Linked fish config"
-
-	# Install fish functions folder
 	fish_func_src="$DOTFILES_ROOT/fish/functions"
 	fish_func_dst="$HOME/.config/fish/functions"
 
-	info "Installing fish functions"
+	info "Linking fish dotfiles"
+	link_file "$fish_src" "$fish_dst"
 	link_file "$fish_func_src" "$fish_func_dst"
-	success "Installing fish functions"
+	success "Linking fish dotfiles"
 }
 
 install_dotfiles() {
@@ -102,7 +103,7 @@ install_dotfiles() {
 	    dst="$HOME/.$(basename "${src%.*}")"
 	    info "Linking $src to $dst"
 	    link_file "$src" "$dst"
-	    success "Linked $src to $dst"
+	    success "Linking $src to $dst"
 	done
 }
 
