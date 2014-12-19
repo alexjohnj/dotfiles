041aa4974a9bf9f1e65e801ab23aff4bc790d80e
Alex Jackson <alex@alexj.org>
2014-12-19 10:29:04 +0000

    Made fish exports a bit more cross platform friendly

--- fish/functions/exports.fish
+++ fish/functions/exports.fish
@@ -5,13 +5,23 @@ set -gx EDITOR vim
 set -gx GOPATH $HOME/.go
 
 # Set up node
-set -gx NODE_PATH /usr/local/lib/node
+if type "node" > /dev/null
+  set -gx NODE_PATH /usr/local/lib/node
+end
 
 # Set up Homebrew & Casks
-set -gx HOMEBREW_CASK_OPTS --appdir=/Applications
+if type "brew" > /dev/null
+  set -gx HOMEBREW_CASK_OPTS --appdir=/Applications
+end
 
 # Octave Configuration
 set -gx GNUTERM "x11" 
 
 # Set the PATH
-set -gx PATH $GOPATH/bin /Applications/MATLAB_R2014a.app/bin/maci64 /usr/local/share/npm/bin /usr/local/opt/ruby/bin /usr/local/sbin /usr/local/bin $PATH
+set pathComponents /Applications/MATLAB_R2014a.app/bin/maci64 /usr/local/share/npm/bin /usr/local/opt/ruby/bin /usr/local/bin /usr/local/sbin /usr/local/lib
+
+for component in $pathComponents[-1..1]
+  if test -e $component
+    set -gx PATH $component $PATH
+  end
+end
