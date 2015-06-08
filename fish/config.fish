032e2dd61d2c156459c61da189030ad3e71bf16a
Alex Jackson <alex@alexj.org>
2015-06-08 11:52:36 +0100

    Changed fish prompt to bobthefish

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@ set fish_path $HOME/.oh-my-fish
 . $fish_path/oh-my-fish.fish
 
 # Theme
-Theme agnoster
+Theme bobthefish
 
 . "$HOME/.config/fish/functions/aliases.fish" # Load Aliases
 . "$HOME/.config/fish/functions/exports.fish" # Load Exports
