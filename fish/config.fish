af21bcf193364904aa69edc8b0e5dac5e4312e8f
Alex Jackson <alex@alexj.org>
2015-06-16 22:25:03 +0100

    Updated fish prompt config

--- fish/config.fish
+++ fish/config.fish
@@ -2,8 +2,14 @@
 set fish_path $HOME/.oh-my-fish
 . $fish_path/oh-my-fish.fish
 
-# Theme
+# Theme Configuraiton
 Theme bobthefish
+set -g theme_display_git yes
+set -g theme_display_hg no
+set -g theme_display_virtualenv no
+set -g theme_display_ruby no
+set -g theme_display_user yes
+set -g default_user alex
 
 . "$HOME/.config/fish/functions/aliases.fish" # Load Aliases
 . "$HOME/.config/fish/functions/exports.fish" # Load Exports
