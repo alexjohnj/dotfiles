f55d9f7a9b5d621db9afdf02f6c8281d80d9e353
Alex Jackson <alex@alexj.org>
2015-07-07 10:22:22 +0100

    Updated ledger-mode config

--- emacs/emacs.d.symlink/init.el
+++ emacs/emacs.d.symlink/init.el
@@ -73,7 +73,10 @@
 
 (use-package ledger-mode
   :ensure t
-  :config (if (eq system-type 'darwin) (setq ledger-binary-path "/usr/local/bin/ledger")))
+  :config
+  (if (eq system-type 'darwin) (setq ledger-binary-path "/usr/local/bin/ledger"))
+  (add-to-list 'auto-mode-alist '("\\.journal$" . ledger-mode))
+  (setq ledger-post-auto-adjust-amounts t))
 
 (use-package highlight-current-line
   :ensure t
