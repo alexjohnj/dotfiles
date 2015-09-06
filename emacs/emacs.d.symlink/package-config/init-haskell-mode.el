de2d9696059931711c15ac2707d714135b410170
Alex Jackson <alex@alexj.org>
2015-09-06 15:14:58 -0600

    Configure haskell-mode fro interactivity in Emacs

--- emacs/emacs.d.symlink/package-config/init-haskell-mode.el
+++ emacs/emacs.d.symlink/package-config/init-haskell-mode.el
@@ -1,6 +1,10 @@
 (use-package haskell-mode
   :ensure t
   :config
-  (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation))
+  (require 'haskell-interactive-mode)
+  (require 'haskell-process)
+  (custom-set-variables '(haskell-stylish-on-save t))
+  (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+  (add-hook 'haskell-mode-hook 'interactive-haskell-mode))
 
 (provide 'init-haskell-mode)
