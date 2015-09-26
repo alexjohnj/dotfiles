b3e85e15c6ff613100ed92ed4c12e114c66b2841
Alex Jackson <alex@alexj.org>
2015-09-26 09:54:08 -0600

    Change Emacs font to Source Code Pro

--- emacs/emacs.d.symlink/init.el
+++ emacs/emacs.d.symlink/init.el
@@ -75,7 +75,7 @@
 (scroll-bar-mode 0)
 (global-linum-mode t)
 
-(set-face-attribute 'default nil :font "Inconsolata-10")
+(set-face-attribute 'default nil :font "Source Code Pro-10")
 
 (add-to-list 'custom-theme-load-path (expand-file-name "themes/" user-emacs-directory))
 (load-theme 'base16-eighties-dark t)
