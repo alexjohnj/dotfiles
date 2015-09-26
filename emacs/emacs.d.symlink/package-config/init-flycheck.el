b86b342b1fe0e0ce25bf9d8ca99c5307227e1ff7
Alex Jackson <alex@alexj.org>
2015-09-26 11:38:44 -0600

    Update flycheck configuration

--- emacs/emacs.d.symlink/package-config/init-flycheck.el
+++ emacs/emacs.d.symlink/package-config/init-flycheck.el
@@ -1,6 +1,16 @@
 ;;; init-flycheck -- Initialise Flycheck Module
 (use-package flycheck
   :ensure t
-  :config (add-hook 'after-init-hook #'global-flycheck-mode))
+  :config (progn
+            (add-hook 'after-init-hook #'global-flycheck-mode)
+            (setq flycheck-indication-mode nil)))
+
+(use-package flycheck-pos-tip
+  :ensure t
+  :config (progn
+            (eval-after-load 'flycheck
+              '(custom-set-variables
+                '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))))
 
 (provide 'init-flycheck)
+;;; init-flycheck.el ends here
