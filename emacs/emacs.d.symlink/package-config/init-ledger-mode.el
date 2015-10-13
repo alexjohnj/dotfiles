0606858c96002950e8cbf47da050d9045cba0dfe
Alex Jackson <alex@alexj.org>
2015-10-12 19:41:40 -0600

    Disable transaction highlighting in ledger-mode

--- emacs/emacs.d.symlink/package-config/init-ledger-mode.el
+++ emacs/emacs.d.symlink/package-config/init-ledger-mode.el
@@ -3,6 +3,7 @@
   :config
   (if (eq system-type 'darwin) (setq ledger-binary-path "/usr/local/bin/ledger"))
   (add-to-list 'auto-mode-alist '("\\.journal$" . ledger-mode))
-  (setq ledger-post-auto-adjust-amounts t))
+  (setq ledger-post-auto-adjust-amounts t)
+  (setq ledger-highlight-xact-under-point nil))
 
 (provide 'init-ledger-mode)
