d9a3f06f25377f3cb43b4a8c10195feb25d8b7be
Alex Jackson <alex@alexj.org>
2015-10-13 20:39:47 -0600

    Rewrite custom prompt
    Broke the prompt up into discrete functions and rewrote some of the git
    info commands.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -6,70 +6,95 @@ function _git_branch_name
   echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
 end
 
-function _is_git_dirty
-  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+function _git_is_clean --description "Returns 0 if clean, 1 otherwise"
+  _git_has_untracked_files
+  set -l has_untracked_files $status
+
+  # Check for stages and unstaged changes
+  not git diff-index --quiet HEAD
+  set -l has_changes $status
+
+  test \( $has_untracked_files -eq 1 \) -a \( $has_changes -eq 1 \)
+  return $status
 end
 
-function _git_untracked_count
-  echo (command git status -s ^/dev/null | grep "??" ^/dev/null | wc -l | tr -d ' ')
+function _git_has_untracked_files --description "Returns 0 if there are untracked files, 1 otherwise"
+  not test -z (git ls-files --exclude-standard --others ^/dev/null)
+  return $status
 end
 
-function _git_changed_count
-  echo (command git diff --name-only HEAD ^/dev/null | wc -l | tr -d ' ')
+function _git_has_changes --description "Returns 0 if there are changes, 1 otherwise"
+  not git diff-files --quiet ^/dev/null
+  return $status
 end
 
-function _git_staged_count
-  echo (command git diff --cached --numstat ^/dev/null | wc -l | tr -d ' ')
+function _git_has_staged_changes --description "Returns 0 if there are staged changes, 1 otherwise"
+  not git diff-index --quiet --cached HEAD ^/dev/null
+  return $status
+end
+
+function _make_prompt_segment
+  set -l bg $argv[1]
+  set -l fg $argv[2]
+
+  set_color -b $bg
+  set_color $fg
+
+  if [ -n "$argv[3]" ]
+    echo -n -s $argv[3]
+  end
+end
+
+function _print_lambda
+  if [ $last_status -eq 0 ]
+    _make_prompt_segment normal blue "λ "
+  else
+    _make_prompt_segment normal red "λ "
+  end
+end
+
+function _print_cwd
+  _make_prompt_segment normal normal (prompt_pwd)" "
+end
+
+function _print_git_status
+  set -l git_branch (_git_branch_name)
+
+  if not [ $git_branch ]
+    return
+  end
+
+  set git_info $git_branch
+
+  if  _git_is_clean
+    _make_prompt_segment green white " $git_info "
+    return
+  end
+
+  if _git_has_untracked_files
+    set git_info "$git_info ⦁"
+  end
+
+  if _git_has_changes
+    set git_info "$git_info ▲"
+  end
+
+  if _git_has_staged_changes
+    set git_info "$git_info ✚"
+  end
+
+  _make_prompt_segment red white " $git_info "
+end
+
+function _print_arrow
+  _make_prompt_segment normal blue " ❯ "
 end
 
 function fish_prompt
-  set -l last_stat $status
-  set -l red (set_color red)
-  set -l blue (set_color blue)
-  set -l green (set_color green)
-  set -l normal (set_color normal)
-  set -l bggreen (set_color --bold --background green white)
-  set -l bgred (set_color --bold --background red white)
-
-  set -l lambda
-  if [ $last_stat -ne 0 ]
-    set lambda $red"λ"
-  else
-    set lambda $blue"λ"
-  end
+  set -g last_status $status
   
-  set -l arrow $blue"❯❯"
-  set -l cwd $normal(prompt_pwd)
-
-  set -l git_info
-  if [ (_git_branch_name) ]
-    set -l git_changed_count (_git_changed_count)
-    set -l git_staged_count (_git_staged_count)
-    set -l git_untracked_count (_git_untracked_count)
-    
-    set git_info (_git_branch_name)
-
-    # Get the right spacing after the branch name
-    if [ $git_untracked_count -gt 0 ]
-      set git_info "$git_info ⦁"
-    end
-
-    if [ $git_changed_count -gt 0 ]
-      set git_info "$git_info ▲"
-    end
-
-    if [ $git_staged_count -gt 0 ]
-      set git_info "$git_info ✚"
-    end
-    
-    if [ (_is_git_dirty) ]
-      set git_info $bgred"["$git_info"]$normal "
-    else
-      set git_info $bggreen"["$git_info"]$normal "
-    end
-  else
-    set git_info ""
-  end
-
-  echo -n -s $lambda ' ' $cwd ' ' $git_info $arrow ' ' $normal
+  _print_lambda
+  _print_cwd
+  _print_git_status
+  _print_arrow
 end
