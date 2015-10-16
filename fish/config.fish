0f41601e59c5bf5688f9bb79d79d25ce6e83f366
Alex Jackson <alex@alexj.org>
2015-10-15 19:34:30 -0600

    Change fish config to load exports before aliases

--- fish/config.fish
+++ fish/config.fish
@@ -5,5 +5,5 @@ set -g theme_display_ruby no
 set -g theme_display_user yes
 set -g default_user alex
 
-. "$HOME/.config/fish/functions/aliases.fish" # Load Aliases
 . "$HOME/.config/fish/functions/exports.fish" # Load Exports
+. "$HOME/.config/fish/functions/aliases.fish" # Load Aliases
