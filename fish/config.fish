b8a6a1881ff6e3cf418bf79faaba1b06d147fe2b
Alex Jackson <alex@alexj.org>
2015-10-15 19:34:57 -0600

    Remove unused env vars from fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,2 @@
-set -g theme_display_git yes
-set -g theme_display_hg no
-set -g theme_display_virtualenv no
-set -g theme_display_ruby no
-set -g theme_display_user yes
-set -g default_user alex
-
 . "$HOME/.config/fish/functions/exports.fish" # Load Exports
 . "$HOME/.config/fish/functions/aliases.fish" # Load Aliases
