7077ea289929c5712f57f8f91a6b4f9168619f36
Alex Jackson <alex@alexj.org>
2015-10-22 15:04:33 -0600

    Add documentation to quicklatexmk script

--- fish/functions/quicklatexmk.fish
+++ fish/functions/quicklatexmk.fish
@@ -1,3 +1,18 @@
+# This little script compiles a target LaTeX file into a PDF using
+# latexmk and (by default) lualatex. This is handy when you want to
+# compile a simple document without setting up a Makefile. Output
+# files are written to your $TMPDIR while the final PDF is copied to
+# your current working directory.
+#
+# Usage:
+#   quicklatexmk [latex-opts] FILE
+#
+# [latex-opts] are passed to latexmk's `-pdflatex=` argument. By
+# default these are:
+#   lualatex --file-line-error --interaction=nonstopmode
+# If, for example, you'd rather use xelatex, you could run:
+#   quicklatexmk xelatex FILE
+
 function quicklatexmk -d "Quickly compile a latex file using latexmk"
   if [ (count $argv) -lt 1 ]
     printf "Usage:\n \t quicklatexmk [latex-options] FILE\n" 
@@ -7,6 +22,7 @@ function quicklatexmk -d "Quickly compile a latex file using latexmk"
   set -l FILE_NAME
   set -l LATEX_ARGS "lualatex --file-line-error --interaction=nonstopmode"
 
+  # Parse arguments
   if [ (count $argv) -eq 1 ]
     set FILE_NAME $argv[1]
   else
@@ -19,8 +35,7 @@ function quicklatexmk -d "Quickly compile a latex file using latexmk"
     return 1
   end
 
-  # TODO: Make output go to $TMP/$FILE/ directory (Check out mktemp(1) or $TMPDIR)
-
+  # Create temporary directory for LaTeX output
   set -l OUTPUT_DIR "$TMPDIR/$FILE_NAME""_Output"
   echo $OUTPUT_DIR
   if not [ -d "$OUTPUT_DIR" ]
@@ -37,6 +52,8 @@ function quicklatexmk -d "Quickly compile a latex file using latexmk"
     return 1
   end
 
+  # Copy the compiled PDF from the temporary directory to the current
+  # working directory
   set -l FILE_BASENAME (basename $FILE_NAME | sed 's/\.[^.]*$//')
   set -l PDF_OUTPUT "$OUTPUT_DIR/$FILE_BASENAME.pdf"
   cp "$PDF_OUTPUT" .
