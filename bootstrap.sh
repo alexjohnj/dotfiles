bfb1d7f7a4cd0fa8f86a111057f9dec5a17d8672
Alex Jackson <alex@alexj.org>
2015-10-24 16:18:27 -0600

    Rewrite bootstrap script for sh
    The script's been rewritten for sh rather than bash. In addition, it no
    longer tries to install fish on Ubuntu or Arch Linux, only OS X. There's
    also now a prompt before installation warning you that it'll overwrite
    any preexisting dotfiles.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,82 +1,132 @@
-#!/usr/bin/env bash
-#
-# Based on @holman's bootstrap file: https://github.com/holman/dotfiles
-# WARNING. Unlike @holman's script, this script will overwrite anything without asking the user.
-# This installs Homebrew, fish shell and dotfiles in the repo
-# After running this script, you'll need to run chsh to make fish your default shell
+#!/bin/sh
+# Based on @holman's bootstrap file:
+# https://github.com/holman/dotfiles WARNING. Unlike @holman's script,
+# this script will overwrite previously installed dotfiles. This
+# installs Homebrew, fish shell and dotfiles in the repo After running
+# this script, you'll need to run chsh to make fish your default shell.
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")" || exit
 DOTFILES_ROOT=$(pwd)
 
-set -e
-
-success () {
-  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+success() {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] %s\n" "$1"
 }
 
-info () {
-  printf "  [ \033[00;34m..\033[0m ] $1"
+info() {
+  printf "\r\033[2k  [ \033[00;34m..\033[0m ] %s" "$1"
 }
 
-fail () {
-  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+fail() {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] %s\n" "$1"
   echo ''
   exit
 }
 
+fail_c() {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] %s\n" "$1"
+}
+
 link_file() {
-	local src=$1 dst=$2
+    local src=$1 dst=$2
 
-	# Make sure parent directories exist
+    if [ ! -d "$(dirname "$dst")" ]
+    then
+        mkdir -p "$(dirname "$dst")"
+    fi
 
-	if [ ! -d $(dirname "$dst") ]
-	then
-		mkdir -p $(dirname "$dst")
-	fi
+    if [ -f "$dst" ] || [ -d "$dst" ] || [ -L "$dst" ]
+    then
+        rm -rf "$dst" || exit
+    fi
+    ln -s "$src" "$dst" || exit
+}
 
-	if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]
-	then
-		rm -rf "$dst"
-	fi
-
-	ln -s "$src" "$dst"
+install_dotfiles() {
+    for src in $(find "$DOTFILES_ROOT" -maxdepth 2 -name "*.symlink")
+    do
+        dst="$HOME/.$(basename "${src%.symlink}")"
+        link_file "$src" "$dst"
+        if [ $? -eq 1 ]
+        then
+           fail_c "Linking $src to $dst"
+        else
+            success "Linking $src to $dst"
+        fi
+    done
 }
 
 install_homebrew() {
-	info "Installing Homebrew"
-	if [ $(uname -s) = "Darwin" ];then
-		if ! type "brew" > /dev/null 2>&1;then
-			ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-			success "Installing Homebrew"
-		else
-			success "Installing Homebrew (already installed)"
-		fi
-	else
-		success "Not Installing Homebrew (incompatible platform)"
-	fi
+    if ! [ "$(uname -s)" = "Darwin" ] || type "brew" > /dev/null 2>&1
+    then
+        return
+    fi
+
+    while true
+    do
+        printf "Install homebrew? [y/n] > "
+        read yn
+        case $yn in
+             [Yy]* ) break;;
+             [Nn]* ) return;;
+        esac
+    done
+
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+    if [ $? -ne 0 ]
+    then
+       info "Failed to install homebrew"
+       fail_c "Failed to install homebrew"
+    else
+        info "Installed homebrew"
+        success "Installed homebrew"
+    fi
 }
 
 install_fish() {
-	# Install fish if it isn't already
-	info "Installing fish"
-	if ! type "fish" > /dev/null 2>&1;then
-		if [ $(uname -s) = "Darwin" ] && type "brew" > /dev/null 2>&1;then
-			brew install fish
-			success "Installing fish"
-		elif [ $(uname -s) = "Linux" ] && type "pacman" > /dev/null 2>&1;then
-			sudo pacman -S --noconfirm fish
-			success "Installing fish"
-		elif [ $(uname -s) = "Linux" ] && type "apt-get" > /dev/null 2>&1;then
-			sudo apt-get install fish
-			success "Installing fish"
-		else
-			fail "Installing fish (install manually)"
-		fi
-	else
-		success "Installing fish (already installed)"
-	fi
+    if type "fish" > /dev/null 2>&1
+    then
+        return
+    fi
 
-	# Install fish dotfiles
+    if ! [ $(uname -s) = "Darwin" ] || ! type "brew" > /dev/null 2>&1
+    then
+        return
+    fi
+
+    while true
+    do
+        printf "Install fish? [y/n] > "
+        read yn
+        case $yn in
+             [Yy]* ) break;;
+             [Nn]* ) return;;
+        esac
+    done
+
+    brew install fish
+    if [ $? -ne 0 ]
+    then
+        info "Failed to install fish"
+        fail_c "Failed to install fish"
+    else
+        info "Installed fish"
+        success "Installed fish"
+    fi
+        
+}
+
+install_fish_dotfiles() {
+    while true
+    do
+        printf "Install fish dotfiles? [y/n] > "
+        read yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) return;;
+        esac
+    done
+
 	fish_src="$DOTFILES_ROOT/fish/config.fish"
 	fish_dst="$HOME/.config/fish/config.fish"
 	fish_func_src="$DOTFILES_ROOT/fish/functions"
@@ -88,24 +138,18 @@ install_fish() {
 	success "Linking fish dotfiles"
 }
 
-install_dotfiles() {
-	for src in $(find "$DOTFILES_ROOT" -maxdepth 2 -name "*.symlink")
-	do
-	    dst="$HOME/.$(basename "${src%.*}")"
-	    info "Linking $src to $dst"
-	    link_file "$src" "$dst"
-	    success "Linking $src to $dst"
-	done
-}
+printf "\033[0;31mWARNING.\033[0m This script will overwrite any previously installed dotfiles!\nContinue? [y/n] >> "
+while true
+do
+    read yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) printf "Enter [y/n] >> "
+    esac
+done
 
-# Check git is installed
-info "Checking git installation"
-if ! type "git" > /dev/null 2>&1;then
-	fail "Checking git installation"
-	printf "Make sure git is installed and in your PATH\n"
-fi
-success "Checking git installation"
 
-install_homebrew
 install_fish
+install_fish_dotfiles
 install_dotfiles
