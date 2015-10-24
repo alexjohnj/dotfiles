dd7e2ed1fe68c4e1903e568bbc413acccb38e50a
Alex Jackson <alex@alexj.org>
2015-10-24 13:12:13 -0600

    Remove Leeds VPN shell functions

--- fish/functions/aliases.fish
+++ fish/functions/aliases.fish
@@ -28,21 +28,3 @@ alias et "emacsclient -nw -a \"\""
 # Work with encrypted ledger files
 alias eledger "gpg --batch -d -q $LEDGER_FILE | ledger -f - "
 alias ehledger "gpg --batch -d -q $LEDGER_FILE | hledger -f -"
-
-# So my University requires that you connect via its VPN to SSH into the 
-# school's computers. This requires using a POS client that can not be quit
-# by killing the associated process because it restarts itself. Thus, these 
-# functions are necessary. Big thanks to @mevanlc for finding this.
-# (https://gist.github.com/Andrewpk/7558715#comment-1209618) 
-function startleedsvpn
-  sudo launchctl load -w /Library/LaunchDaemons/net.juniper.AccessService.plist
-  launchctl load -w /Library/LaunchAgents/net.juniper.pulsetray.plist
-  echo "Don't forget to run killleedsvpn to kill this POS..."
-end
-
-function killleedsvpn
-  launchctl unload -w /Library/LaunchAgents/net.juniper.pulsetray.plist
-  sudo launchctl unload -w /Library/LaunchDaemons/net.juniper.AccessService.plist
-  osascript -e 'tell application "Junos Pulse" to quit'
-end
-
