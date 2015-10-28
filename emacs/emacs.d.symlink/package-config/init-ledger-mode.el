54b97286b49f226a6344064b0f881f2d39b8ca27
Alex Jackson <alex@alexj.org>
2015-10-28 13:44:23 -0600

    Update ledger-mode configuration
    Added the flycheck-ledger package and added an explicit variable for the
    post alignment column.

--- emacs/emacs.d.symlink/package-config/init-ledger-mode.el
+++ emacs/emacs.d.symlink/package-config/init-ledger-mode.el
@@ -1,9 +1,13 @@
 (use-package ledger-mode
   :ensure t
-  :config
-  (if (eq system-type 'darwin) (setq ledger-binary-path "/usr/local/bin/ledger"))
-  (add-to-list 'auto-mode-alist '("\\.journal$" . ledger-mode))
-  (setq ledger-post-auto-adjust-amounts t)
-  (setq ledger-highlight-xact-under-point nil))
+  :defer t
+  :mode ("\\.journal$" . ledger-mode)
+  :config (progn
+            (if (eq system-type 'darwin) (setq ledger-binary-path "/usr/local/bin/ledger"))
+            (setq ledger-post-amount-alignment-column 60)
+            (setq ledger-post-auto-adjust-amounts t)
+            (setq ledger-highlight-xact-under-point nil)))
+
+(use-package flycheck-ledger :ensure t :defer t)
 
 (provide 'init-ledger-mode)
