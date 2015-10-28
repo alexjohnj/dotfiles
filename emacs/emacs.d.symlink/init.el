dc366ba2ef0be4b713d97ba81540b356dcb3edb6
Alex Jackson <alex@alexj.org>
2015-10-28 16:25:18 -0600

    Change Emacs theme to zenburn

--- emacs/emacs.d.symlink/init.el
+++ emacs/emacs.d.symlink/init.el
@@ -92,12 +92,17 @@
   (menu-bar-mode 0)
   (scroll-bar-mode 0))
 
-(set-face-attribute 'default nil :font "Source Code Pro-10")
+(set-face-attribute 'default nil :font "Source Code Pro-11")
 
-(use-package ample-theme
+(use-package zenburn-theme
   :ensure t
   :defer t
-  :init (progn (load-theme 'ample t)))
+  :init (progn (load-theme 'zenburn t)))
+
+;; (use-package ample-theme
+;;   :ensure t
+;;   :defer t
+;;   :init (progn (load-theme 'ample t)))
 
 ;; Match fringe colour to background colour
 (set-face-attribute 'fringe nil
@@ -105,7 +110,6 @@
                     :background (face-background 'default))
 
 (global-hl-line-mode 1)
-(set-face-background hl-line-face "#2b2b2b")
 
 ;;------------------------------------------------------------------------------
 ;;                  Load Packages that need configuring
