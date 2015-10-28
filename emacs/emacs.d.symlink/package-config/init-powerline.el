977ad7ac42c5a2f6c18beff21082de8c7296d493
Alex Jackson <alex@alexj.org>
2015-10-27 21:13:38 -0600

    Remove powerline SRGB offset function

--- emacs/emacs.d.symlink/package-config/init-powerline.el
+++ emacs/emacs.d.symlink/package-config/init-powerline.el
@@ -1,17 +1,8 @@
 (use-package powerline
   :ensure t
-  :config
-  (if (eq system-type 'darwin)
-      (progn
-        (load-library "powerline-srgb-offset")
-        (powerline-srgb-offset-add-theme "base16-eighties"
-                                         '('("mode-line" "#646464")
-                                           '("powerline-active1" "#484848")
-                                           '("powerline-active2" "#797979")
-                                           '("mode-line-inactive" "#494949")
-                                           '("powerline-inactive1" "#242424")
-                                           '("powerline-inactive2" "#424242")))
-        (powerline-srgb-offset-activate "base16-eighties")))
-  (powerline-default-theme))
+  :config (progn
+            (when (display-graphic-p)
+              (setq powerline-default-separator 'contour))
+            (powerline-default-theme)))
 
 (provide 'init-powerline)
