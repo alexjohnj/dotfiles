7b12df5d7b513a7eb55b17e364635ef72a0cb6a2
Alex Jackson <alex@alexj.org>
2015-10-28 16:15:54 -0600

    Add evil-leader bindings for projectile

--- emacs/emacs.d.symlink/package-config/init-projectile.el
+++ emacs/emacs.d.symlink/package-config/init-projectile.el
@@ -1,6 +1,16 @@
 (use-package projectile
   :ensure t
+  :init (progn
+          (setq projectile-switch-project-action 'projectile-dired)
+          (evil-leader/set-key
+            "pb" 'projectile-switch-to-buffer
+            "pd" 'projectile-find-dir
+            "pf" 'projectile-find-file
+            "pp" 'projectile-switch-project
+            "pb" 'projectile-switch-to-buffer
+            "pk" 'projectile-kill-buffers
+            "pD" 'projectile-dired))
   :config (progn
-            (projectile-global-mode)
-            (setq projectile-switch-project-action 'projectile-dired)))
+            (projectile-global-mode)))
+
 (provide 'init-projectile)
