e9742d1f7d569791163fbe1cbcfbd1fb0a74b32e
Alex Jackson <alex@alexj.org>
2015-10-27 21:13:54 -0600

    Disable file caching in projectile

--- emacs/emacs.d.symlink/package-config/init-projectile.el
+++ emacs/emacs.d.symlink/package-config/init-projectile.el
@@ -1,7 +1,6 @@
 (use-package projectile
   :ensure t
-  :config
-  (projectile-global-mode)
-  (setq projectile-enable-caching t)
-  (setq projectile-switch-project-action 'projectile-dired))
+  :config (progn
+            (projectile-global-mode)
+            (setq projectile-switch-project-action 'projectile-dired)))
 (provide 'init-projectile)
