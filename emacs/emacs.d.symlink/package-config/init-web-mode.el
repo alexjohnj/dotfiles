6f8cf17403187eb81492f4bd91b3248bc5458192
Alex Jackson <alex@alexj.org>
2015-10-28 20:07:27 -0600

    Update web-mode configuration

--- emacs/emacs.d.symlink/package-config/init-web-mode.el
+++ emacs/emacs.d.symlink/package-config/init-web-mode.el
@@ -1,15 +1,12 @@
 (use-package web-mode
   :ensure t
-  :config
-  (defun my-web-mode-hook ()
-    (setq web-mode-markup-indent-offset 2)
-    (setq web-mode-css-indent-offset 2)
-    (setq web-mode-code-indent-offset 2)
-    (setq web-mode-enable-auto-pairing t)
-    (setq web-mode-enable-engine-detection t))
-
-  (setq web-mode-enable-current-element-highlight t)
-  (add-hook 'web-mode-hook 'my-web-mode-hook)
-  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode)))
+  :mode ("\\.html" . web-mode)
+  :init (progn
+          (setq web-mode-markup-indent-offset 2
+                web-mode-css-indent-offset 2
+                web-mode-code-indent-offset 2
+                web-mode-enable-auto-pairing t
+                web-mode-enable-engine-detection t
+                web-mode-enable-current-element-highlight t)))
 
 (provide 'init-web-mode)
