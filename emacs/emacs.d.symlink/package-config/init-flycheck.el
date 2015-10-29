2b038413802481b77656464a838ea2ec91e07014
Alex Jackson <alex@alexj.org>
2015-10-28 18:39:02 -0600

    Customise Flycheck configuration
    Added a bunch of evil-leader bindings, reenabled the gutter markings and
    improved how the package is loaded with use-package.

--- emacs/emacs.d.symlink/package-config/init-flycheck.el
+++ emacs/emacs.d.symlink/package-config/init-flycheck.el
@@ -1,16 +1,27 @@
 ;;; init-flycheck -- Initialise Flycheck Module
 (use-package flycheck
   :ensure t
+  :diminish flycheck-mode
+  :init (progn
+          (setq flycheck-indication-mode 'left-fringe
+                flycheck-display-errors-delay 0.5)
+          (evil-leader/set-key
+            "ec" 'flycheck-buffer
+            "en" 'flycheck-next-error
+            "eN" 'flycheck-previous-error
+            "e1" 'flycheck-first-error
+            "eC" 'flycheck-clear
+            "el" 'flycheck-list-errors
+            "ev" 'flycheck-verify-setup
+            "et" 'flycheck-mode))
   :config (progn
-            (add-hook 'after-init-hook #'global-flycheck-mode)
-            (setq flycheck-indication-mode nil)))
+            (add-hook 'after-init-hook #'global-flycheck-mode)))
 
 (use-package flycheck-pos-tip
   :ensure t
-  :config (progn
-            (eval-after-load 'flycheck
-              '(custom-set-variables
-                '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))))
+  :defer t
+  :init (progn
+          (setq flycheck-display-errors-function 'flycheck-pos-tip-error-messages)))
 
 (provide 'init-flycheck)
 ;;; init-flycheck.el ends here
