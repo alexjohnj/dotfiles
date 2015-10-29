6ebe883f212ab5b09cc23a51c33283c746b13a94
Alex Jackson <alex@alexj.org>
2015-10-28 20:02:15 -0600

    Update scss-mode package to use use-package

--- emacs/emacs.d.symlink/package-config/init-scss-mode.el
+++ emacs/emacs.d.symlink/package-config/init-scss-mode.el
@@ -1,4 +1,7 @@
-(autoload 'scss-mode "scss-mode")
-(setq scss-compile-at-save nil)
-(add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+(use-package scss-mode
+  :ensure t
+  :mode ("\\.scss" . scss-mode)
+  :init (progn
+          (setq scss-compile-at-save nil)))
+
 (provide 'init-scss-mode)
