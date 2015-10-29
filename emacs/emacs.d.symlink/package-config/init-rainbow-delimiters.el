1d446f0356b6e00c4410b7bb5f2f34821c22e003
Alex Jackson <alex@alexj.org>
2015-10-28 19:00:08 -0600

    Update rainbow-delimiters package config

--- emacs/emacs.d.symlink/package-config/init-rainbow-delimiters.el
+++ emacs/emacs.d.symlink/package-config/init-rainbow-delimiters.el
@@ -1,6 +1,8 @@
 (use-package rainbow-delimiters
   :ensure t
-  :config
-  (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
-  (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode))
+  :commands (rainbow-delimiters-mode)
+  :init (progn
+          (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+          (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)))
+
 (provide 'init-rainbow-delimiters)
