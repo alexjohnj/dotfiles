cb33d103c6735c51fe62eb0b8fc78087b9c87698
Alex Jackson <alex@alexj.org>
2015-10-28 19:08:08 -0600

    Tidy up ledger configuration

--- emacs/emacs.d.symlink/package-config/init-ledger-mode.el
+++ emacs/emacs.d.symlink/package-config/init-ledger-mode.el
@@ -1,12 +1,11 @@
 (use-package ledger-mode
   :ensure t
-  :defer t
   :mode ("\\.journal$" . ledger-mode)
-  :config (progn
+  :init (progn
             (if (eq system-type 'darwin) (setq ledger-binary-path "/usr/local/bin/ledger"))
-            (setq ledger-post-amount-alignment-column 60)
-            (setq ledger-post-auto-adjust-amounts t)
-            (setq ledger-highlight-xact-under-point nil)))
+            (setq ledger-post-amount-alignment-column 60
+                  ledger-post-auto-adjust-amounts t
+                  ledger-highlight-xact-under-point nil)))
 
 (use-package flycheck-ledger :ensure t :defer t)
 
