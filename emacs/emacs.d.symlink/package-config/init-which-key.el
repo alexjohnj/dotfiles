8462367d0fc755e7411d63823c7aecce61c8ab06
Alex Jackson <alex@alexj.org>
2015-10-28 19:05:22 -0600

    Fix which-key package config

--- emacs/emacs.d.symlink/package-config/init-which-key.el
+++ emacs/emacs.d.symlink/package-config/init-which-key.el
@@ -1,8 +1,8 @@
 (use-package which-key
   :ensure t
-  :defer t
-  :config (progn
-            (which-key-mode)
-            (which-key-setup-side-window-right-bottom)
-            (setq which-key-idle-delay 0.5)))
+  :init (progn
+          (which-key-setup-side-window-right-bottom)
+          (setq which-key-idle-delay 0.3)
+          (which-key-mode)))
+
 (provide 'init-which-key)
