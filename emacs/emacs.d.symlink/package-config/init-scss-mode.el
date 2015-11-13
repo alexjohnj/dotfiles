6d86d6e35185f773adb417799cad51193ca3f36e
Alex Jackson <alex@alexj.org>
2015-11-13 10:51:26 -0700

    Remove local copy of scss-mode

--- emacs/emacs.d.symlink/package-config/init-scss-mode.el
+++ emacs/emacs.d.symlink/package-config/init-scss-mode.el
@@ -1,7 +1,12 @@
+;;; init-scss-mode --- Configuration for scss-mode
+;;; Commentary:
+;;  Uses the package from https://github.com/antonj/scss-mode
+;;; Code:
 (use-package scss-mode
   :ensure t
-  :mode ("\\.scss\\'" . scss-mode)
+  :mode (("\\.scss\\'" . scss-mode))
   :init (progn
           (setq scss-compile-at-save nil)))
 
 (provide 'init-scss-mode)
+;;; init-scss-mode.el ends here
