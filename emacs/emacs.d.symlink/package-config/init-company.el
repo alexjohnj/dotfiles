ace41d8d5a6063618ece5ceda8356ae43d77f438
Alex Jackson <alex@alexj.org>
2015-12-27 09:40:57 -0700

    Add company-emoji package to Emacs
    😄

--- emacs/emacs.d.symlink/package-config/init-company.el
+++ emacs/emacs.d.symlink/package-config/init-company.el
@@ -6,4 +6,9 @@
                         company-show-numbers t)
                  (add-hook 'after-init-hook 'global-company-mode)))
 
+(use-package company-emoji
+  :ensure t
+  :init (progn
+          (add-to-list 'company-backends 'company-emoji)))
+
 (provide 'init-company)
