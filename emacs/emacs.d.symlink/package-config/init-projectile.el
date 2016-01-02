5cfb014352a0703d607f8cdf2f7ac0cf87062327
Alex Jackson <alex@alexj.org>
2016-01-02 15:38:46 -0700

    Add new evil-leader keybindings for projectile

--- emacs/emacs.d.symlink/package-config/init-projectile.el
+++ emacs/emacs.d.symlink/package-config/init-projectile.el
@@ -8,9 +8,11 @@
             "pd" 'projectile-find-dir
             "pf" 'projectile-find-file
             "pp" 'projectile-switch-project
+            "pq" 'projectile-switch-open-project
             "pb" 'projectile-switch-to-buffer
             "pk" 'projectile-kill-buffers
-            "pD" 'projectile-dired)
+            "pD" 'projectile-dired
+            "pg" 'projectile-grep)
           (which-key-add-key-based-replacements "SPC p" "Project"))
   :config (progn
             (projectile-global-mode)))
