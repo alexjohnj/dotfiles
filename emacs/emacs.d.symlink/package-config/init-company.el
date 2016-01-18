492a35de5297342221cb5010c7feb2ea3e5c9979
Alex Jackson <alex@alexj.org>
2016-01-17 17:39:36 -0700

    Remove company-emoji package

--- emacs/emacs.d.symlink/package-config/init-company.el
+++ emacs/emacs.d.symlink/package-config/init-company.el
@@ -2,13 +2,8 @@
   :ensure t
   :diminish company-mode
   :config (progn (setq company-idle-delay 0.1
-                        company-minimum-prefix-length 2
-                        company-show-numbers t)
+                       company-minimum-prefix-length 2
+                       company-show-numbers t)
                  (add-hook 'after-init-hook 'global-company-mode)))
-
-(use-package company-emoji
-  :ensure t
-  :init (progn
-          (add-to-list 'company-backends 'company-emoji)))
 
 (provide 'init-company)
