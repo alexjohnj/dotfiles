adc9972d0f5b77a6a33e8419e661ee66b3a10648
Alex Jackson <alex@alexj.org>
2016-03-05 09:33:24 -0700

    Add new evil-leader bindings for projectile

--- emacs/emacs.d.symlink/package-config/init-projectile.el
+++ emacs/emacs.d.symlink/package-config/init-projectile.el
@@ -9,12 +9,21 @@
             "pf" 'projectile-find-file
             "pp" 'projectile-switch-project
             "pq" 'projectile-switch-open-project
-            "pb" 'projectile-switch-to-buffer
             "pk" 'projectile-kill-buffers
             "pD" 'projectile-dired
-            "pg" 'projectile-grep)
+            "psg" 'projectile-grep
+            "pss" 'projectile-ag
+            "pS" 'projectile-save-project-buffers
+            "p-" 'alex/projectile-remove-known-project)
           (which-key-add-key-based-replacements "SPC p" "Project"))
   :config (progn
             (projectile-global-mode)))
+
+(defun alex/projectile-remove-known-project ()
+  "Removes the current project if you're in one, otherwise prompts for a project to remove."
+  (interactive)
+  (if (projectile-project-p)
+      (projectile-remove-current-project-from-known-projects)
+    (call-interactively 'projectile-remove-known-project)))
 
 (provide 'init-projectile)
