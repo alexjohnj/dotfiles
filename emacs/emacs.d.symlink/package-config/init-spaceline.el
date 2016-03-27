e8cdf690ba89df63f4bf65c66f27e1050a8e71a5
Alex Jackson <alex@alexj.org>
2016-03-27 08:00:47 -0600

    Change powerline separator

--- emacs/emacs.d.symlink/package-config/init-spaceline.el
+++ emacs/emacs.d.symlink/package-config/init-spaceline.el
@@ -2,7 +2,7 @@
   :ensure t
   :config (progn
             (require 'spaceline-config)
-            (setq powerline-default-separator 'wave)
+            (setq powerline-default-separator 'bar)
             (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state)
             (spaceline-spacemacs-theme)))
 
