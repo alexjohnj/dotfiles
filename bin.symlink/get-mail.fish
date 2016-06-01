a88a472a00831ba8805f5ca5fc8e31642ebf8a8d
Alex Jackson <alex@alexj.org>
2016-06-01 11:08:52 +0100

    Update get-mail script and launchd agent

--- bin.symlink/get-mail.fish
+++ bin.symlink/get-mail.fish
@@ -4,18 +4,20 @@ function log
   logger -s -t "[mail-sync]" $argv
 end
 
-# Make a folder for logs from launchd
+log "Mail sync started."
+
+# Log folder
 if not test -d "/Users/alex/Library/Logs/get-mail"
   mkdir -p "/Users/alex/Library/Logs/get-mail"
 end
 
 if pgrep mbsync
-  log "Killing previous mbsync processes."
-  pkill mbsync
+  log "mbsync in process. Skipping this sync."
+  exit 2
 end
 
 log "Starting mbsync..."
-/usr/local/bin/mbsync -a
+/usr/local/bin/mbsync -a -q
 log "Starting mu..."
 /usr/local/bin/mu index -q --maildir=/Users/alex/.mail
 log "Mail sync finished."
