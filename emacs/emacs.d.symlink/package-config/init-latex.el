9d7067513071f90ca1ef122550c00339c695207f
Alex Jackson <alex@alexj.org>
2016-07-06 17:56:40 +0100

    Optimise Emacs LaTeX configuration for startup

--- emacs/emacs.d.symlink/package-config/init-latex.el
+++ emacs/emacs.d.symlink/package-config/init-latex.el
@@ -1,23 +1,23 @@
 ;;; init-latex --- LaTeX Editing Modes
 ;;; Commentary:
 ;;; Code:
-(use-package auctex
-  :ensure t
-  :mode ("\\.tex\\'" . latex-mode)
-  :commands (latex-mode LaTeX-mode plain-tex-mode)
-  :init (progn
-          (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
-          (add-hook 'TeX-mode-hook 'turn-on-auto-fill)
-          (setq-default TeX-engine 'lualatex)
-          (setq-default TeX-master nil)
-          (setq TeX-auto-save t
-                TeX-parse-self t
-                TeX-electric-math (cons "\\(" "\\)")
-                LaTeX-electric-math-left-right-brace t
-                LaTeX-electric-left-right-brace t
-                TeX-electric-sub-and-superscript t
-                LaTeX-fill-break-at-separators '(\\\( \\\[))
-          (evil-leader/set-key-for-mode 'latex-mode
+(use-package tex-site
+  :ensure auctex
+  :mode ("\\.tex\\'" . LaTeX-mode)
+  :config (progn
+            (message "Hi from auctex")
+            (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+            (add-hook 'TeX-mode-hook 'turn-on-auto-fill)
+            (setq-default TeX-engine 'lualatex)
+            (setq-default TeX-master nil)
+            (setq TeX-auto-save t
+                  TeX-parse-self t
+                  TeX-electric-math (cons "\\(" "\\)")
+                  LaTeX-electric-math-left-right-brace t
+                  LaTeX-electric-left-right-brace t
+                  TeX-electric-sub-and-superscript t
+                  LaTeX-fill-break-at-separators '(\\\( \\\[))
+            (evil-leader/set-key-for-mode 'latex-mode
               "m p e" 'preview-environment
               "m p d" 'preview-document
               "m p b" 'preview-buffer
@@ -30,24 +30,28 @@
 
 (use-package reftex
   :ensure t
+  :commands (turn-on-reftex)
   :init (progn
-          (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-          (setq reftex-plug-into-AUCTeX t)
-          (evil-define-key 'normal reftex-toc-mode-map
-            (kbd "j") 'reftex-toc-next
-            (kbd "k") 'reftex-toc-previous
-            (kbd "Q") 'reftex-toc-quit-and-kill
-            (kbd "%") 'reftex-toc-rename-label)
-          (evil-leader/set-key-for-mode 'latex-mode
-            "m r c" 'reftex-citation
-            "m r r" 'reftex-reference
-            "m r l" 'reftex-label
-            "m r t" 'reftex-toc
-            "m r v" 'reftex-view-crossref)))
+          (add-hook 'LaTeX-mode-hook 'turn-on-reftex))
+  :config (progn
+            (setq reftex-plug-into-AUCTeX t)
+            (evil-define-key 'normal reftex-toc-mode-map
+              (kbd "j") 'reftex-toc-next
+              (kbd "k") 'reftex-toc-previous
+              (kbd "Q") 'reftex-toc-quit-and-kill
+              (kbd "%") 'reftex-toc-rename-label)
+            (evil-leader/set-key-for-mode 'latex-mode
+              "m r c" 'reftex-citation
+              "m r r" 'reftex-reference
+              "m r l" 'reftex-label
+              "m r t" 'reftex-toc
+              "m r v" 'reftex-view-crossref)))
 
 (use-package company-auctex
   :ensure t
-  :config (progn (company-auctex-init)))
+  :after (latex)
+  :config (progn
+            (company-auctex-init)))
 
 (provide 'init-latex)
 ;;; init-latex.el ends here
