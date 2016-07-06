0193d76c4837869abc8253c467cad179e9576ac4
Alex Jackson <alex@alexj.org>
2016-07-06 11:21:23 +0100

    Make Emacs copy GPG_AGENT_INFO and SSH_AGENT_PID on startup

--- emacs/emacs.d.symlink/package-config/init-exec-path-from-shell.el
+++ emacs/emacs.d.symlink/package-config/init-exec-path-from-shell.el
@@ -3,6 +3,8 @@
     :ensure t
     :config (progn
               (exec-path-from-shell-initialize)
-              (exec-path-from-shell-copy-env "SSH_AUTH_SOCK"))))
+              (exec-path-from-shell-copy-env "GPG_AGENT_INFO")
+              (exec-path-from-shell-copy-env "SSH_AUTH_SOCK")
+              (exec-path-from-shell-copy-env "SSH_AGENT_PID"))))
 
 (provide 'init-exec-path-from-shell)
