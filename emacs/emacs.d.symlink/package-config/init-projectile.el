fd16796d5b258bb9dc41210388610ddee47b9e6d
Alex Jackson <alex@alexj.org>
2016-07-06 18:13:56 +0100

    Optimise projectile.el configuration for Emacs start time

--- emacs/emacs.d.symlink/package-config/init-projectile.el
+++ emacs/emacs.d.symlink/package-config/init-projectile.el
@@ -1,23 +1,25 @@
 (use-package projectile
   :ensure t
   :diminish projectile-mode
+  :commands (projectile-switch-project projectile-project-p)
   :init (progn
-          (setq projectile-switch-project-action 'projectile-dired)
           (evil-leader/set-key
-            "pb" 'projectile-switch-to-buffer
-            "pd" 'projectile-find-dir
-            "pf" 'projectile-find-file
             "pp" 'projectile-switch-project
-            "pq" 'projectile-switch-open-project
-            "pk" 'projectile-kill-buffers
-            "pD" 'projectile-dired
-            "pr" 'projectile-replace
-            "psg" 'projectile-grep
-            "pss" 'projectile-ag
-            "pS" 'projectile-save-project-buffers
             "p-" 'alex/projectile-remove-known-project)
           (which-key-add-key-based-replacements "SPC p" "Project"))
   :config (progn
+            (setq projectile-switch-project-action 'projectile-dired)
+            (evil-leader/set-key
+              "pb" 'projectile-switch-to-buffer
+              "pq" 'projectile-switch-open-project
+              "pd" 'projectile-find-dir
+              "pf" 'projectile-find-file
+              "pk" 'projectile-kill-buffers
+              "pD" 'projectile-dired
+              "pr" 'projectile-replace
+              "psg" 'projectile-grep
+              "pss" 'projectile-ag
+              "pS" 'projectile-save-project-buffers)
             (projectile-global-mode)))
 
 (defun alex/projectile-remove-known-project ()
