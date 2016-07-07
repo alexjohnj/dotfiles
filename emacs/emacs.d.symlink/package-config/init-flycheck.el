6aaee898866b0091e1c2ce6804250ddad524d251
Alex Jackson <alex@alexj.org>
2016-07-07 09:44:18 +0100

    Update flycheck configuration

--- emacs/emacs.d.symlink/package-config/init-flycheck.el
+++ emacs/emacs.d.symlink/package-config/init-flycheck.el
@@ -1,22 +1,25 @@
 ;;; init-flycheck -- Initialise Flycheck Module
+;; Code:
+
 (use-package flycheck
   :ensure t
   :diminish flycheck-mode
   :init (progn
-          (setq flycheck-indication-mode 'left-fringe
-                flycheck-display-errors-delay 0.5)
-          (evil-leader/set-key
-            "ec" 'flycheck-buffer
-            "en" 'flycheck-next-error
-            "eN" 'flycheck-previous-error
-            "e1" 'flycheck-first-error
-            "eC" 'flycheck-clear
-            "el" 'flycheck-list-errors
-            "ev" 'flycheck-verify-setup
-            "et" 'flycheck-mode)
-          (which-key-add-key-based-replacements "SPC e" "Flycheck"))
+          (add-hook 'after-init-hook #'global-flycheck-mode))
   :config (progn
-            (add-hook 'after-init-hook #'global-flycheck-mode)
+            (setq flycheck-indication-mode 'left-fringe
+                  flycheck-display-errors-delay 0.5)
+            (evil-leader/set-key
+              "ec" 'flycheck-buffer
+              "en" 'flycheck-next-error
+              "eN" 'flycheck-previous-error
+              "e1" 'flycheck-first-error
+              "eC" 'flycheck-clear
+              "el" 'flycheck-list-errors
+              "ev" 'flycheck-verify-setup
+              "et" 'flycheck-mode)
+            (which-key-add-key-based-replacements "SPC e" "Flycheck")
+
             (flycheck-define-checker proselint
               "A linter for prose."
               :command ("proselint" source-inplace)
@@ -29,9 +32,10 @@
 
 (use-package flycheck-pos-tip
   :ensure t
+  :after flycheck
   :commands (flycheck-pos-tip-error-messages)
-  :init (progn
-          (setq flycheck-display-errors-function 'flycheck-pos-tip-error-messages)))
+  :config (progn
+            (setq flycheck-display-errors-function 'flycheck-pos-tip-error-messages)))
 
 (provide 'init-flycheck)
 ;;; init-flycheck.el ends here
