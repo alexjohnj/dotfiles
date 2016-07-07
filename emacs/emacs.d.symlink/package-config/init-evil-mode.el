f933af7e1a6a3e1665e9154d89ed8c099eb02ed3
Alex Jackson <alex@alexj.org>
2016-07-07 09:32:48 +0100

    Make sure evil-leader is loaded before evil-mode

--- emacs/emacs.d.symlink/package-config/init-evil-mode.el
+++ emacs/emacs.d.symlink/package-config/init-evil-mode.el
@@ -1,3 +1,6 @@
+;;; init-evil-mode.el --- Configuration for evil-mode and related packages
+;;; Code:
+
 (use-package evil-leader
   :ensure t
   :config (progn
@@ -6,21 +9,24 @@
 
 (use-package evil
   :ensure t
+  :after evil-leader
   :diminish undo-tree-mode
-  :config (progn
-            (evil-mode 1)))
+  :config (progn (evil-mode 1)))
 
 (use-package evil-escape
   :ensure t
+  :after evil
   :diminish evil-escape-mode
   :config (progn
             (evil-escape-mode)))
 
 (use-package evil-commentary
   :ensure t
+  :after evil
   :diminish evil-commentary-mode
   :bind (("S-/" . evil-commentary))
   :init (progn
           (evil-commentary-mode)))
 
 (provide 'init-evil-mode)
+;;; init-evil-mode.el ends here
