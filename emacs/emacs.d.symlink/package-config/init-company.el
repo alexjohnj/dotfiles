2eeebc5c588b46754dae2defd19bd228eed67ee7
Alex Jackson <alex@alexj.org>
2016-07-07 10:35:13 +0100

    Update company-mode config

--- emacs/emacs.d.symlink/package-config/init-company.el
+++ emacs/emacs.d.symlink/package-config/init-company.el
@@ -1,9 +1,11 @@
 (use-package company
   :ensure t
   :diminish company-mode
-  :config (progn (setq company-idle-delay 0.1
+  :commands (global-company-mode)
+  :init (progn (add-hook 'after-init-hook 'global-company-mode))
+  :config (progn
+            (setq company-idle-delay 0.1
                        company-minimum-prefix-length 2
-                       company-show-numbers t)
-                 (add-hook 'after-init-hook 'global-company-mode)))
+                       company-show-numbers t)))
 
 (provide 'init-company)
