7082f8500e8769afe259bbc554338b9a85f6cae6
Alex Jackson <alex@alexj.org>
2016-08-29 18:23:14 +0100

    Set up counsel for ivy-mode

--- emacs/emacs.d.symlink/package-config/init-swiper.el
+++ emacs/emacs.d.symlink/package-config/init-swiper.el
@@ -3,15 +3,23 @@
 (use-package swiper
   :ensure t
   :diminish (ivy-mode)
-  :init (progn
-          (setq projectile-completion-system 'ivy)
-          (global-set-key (kbd "C-s") 'swiper)
-          (with-eval-after-load "ivy"
-            (define-key ivy-minibuffer-map (kbd "C-j") 'ivy-next-line)
-            (define-key ivy-minibuffer-map (kbd "C-k") 'ivy-previous-line)
-            (define-key ivy-minibuffer-map [escape] (kbd "C-g"))))
   :config (progn
+            (global-set-key (kbd "C-s") 'swiper)
+            (evil-leader/set-key "s" 'ivy-resume)
+            (setq ivy-use-virtual-buffers t
+                  projectile-completion-system 'ivy)
+            (with-eval-after-load "ivy"
+              (define-key ivy-minibuffer-map (kbd "C-j") 'ivy-next-line)
+              (define-key ivy-minibuffer-map (kbd "C-k") 'ivy-previous-line)
+              (define-key ivy-minibuffer-map [escape] (kbd "C-g")))
             (ivy-mode)))
+
+(use-package counsel
+  :ensure t
+  :config (progn
+            (global-set-key (kbd "C-s") 'counsel-grep-or-swiper)
+            (global-set-key (kbd "M-x") 'counsel-M-x)
+            (global-set-key (kbd "C-x C-f") 'counsel-find-file)))
 
 (provide 'init-swiper)
 ;;; init-swiper.el ends here
