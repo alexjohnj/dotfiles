d9dbf53bbe4e34cac62f814ca07cd6e0dbccb5d3
Alex Jackson <alex@alexj.org>
2016-11-29 08:25:57 +0000

    Update matlab-mode configuration

--- emacs/emacs.d.symlink/package-config/init-matlab-mode.el
+++ emacs/emacs.d.symlink/package-config/init-matlab-mode.el
@@ -1,15 +1,19 @@
 (use-package matlab
   :ensure matlab-mode
   :mode ("\\.m\\'" . matlab-mode)
-  :init (progn
-          (setq matlab-indent-level 2)
-          (evil-leader/set-key-for-mode 'matlab-mode
-            "mcc" 'matlab-ispell-comments
-            "mcs" 'matlab-ispell-strings
-            "ms<" 'matlab-show-matlab-shell-buffer
-            "msg" 'matlab-shell-save-and-go
-            "msv" 'matlab-shell-describe-variable
-            "msh" 'matlab-shell-describe-command
-            "mse" 'matlab-shell-last-error)))
+  :config (progn
+            (setq matlab-indent-level 2
+                  matlab-indent-function-body t
+                  matlab-return-add-semicolon t)
+            (evil-leader/set-key-for-mode 'matlab-mode
+              "m c c" 'matlab-ispell-comments
+              "m c s" 'matlab-ispell-strings
+              "m s s" 'matlab-shell
+              "m s <" 'matlab-show-matlab-shell-buffer
+              "m s g" 'matlab-shell-save-and-go
+              "m s v" 'matlab-shell-describe-variable
+              "m s h" 'matlab-shell-describe-command
+              "m s e" 'matlab-shell-last-error)
+            (add-to-list 'company-backends 'company-matlab-shell)))
 
 (provide 'init-matlab-mode)
