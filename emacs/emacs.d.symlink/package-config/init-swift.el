f36b42bedaf209b76d42e9c95291b78c64caf697
Alex Jackson <alex@alexj.org>
2017-02-11 10:15:27 +0000

    Add flycheck-swift package to Emacs

--- emacs/emacs.d.symlink/package-config/init-swift.el
+++ emacs/emacs.d.symlink/package-config/init-swift.el
@@ -1,4 +1,10 @@
 (use-package swift-mode
   :ensure t
   :mode ("\\.swift\\'" . swift-mode))
+
+(use-package flycheck-swift
+  :ensure t
+  :after (flycheck)
+  :config (flycheck-swift-setup))
+
 (provide 'init-swift)
