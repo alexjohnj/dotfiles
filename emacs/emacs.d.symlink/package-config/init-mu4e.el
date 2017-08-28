6ea864a47edc496fe6a532970ae0253a40732eeb
Alex Jackson <alex@alexj.org>
2017-08-28 10:32:29 +0100

    Remove U of A email configuration

--- emacs/emacs.d.symlink/package-config/init-mu4e.el
+++ emacs/emacs.d.symlink/package-config/init-mu4e.el
@@ -42,16 +42,6 @@
                               (mu4e-refile-folder . "/personal/Archive")
                               (mu4e-sent-messages-behaviour . 'sent)))
                     ,(make-mu4e-context
-                      :name "Alberta"
-                      :match-func (lambda (msg) (alex/mu4e-match-account msg "uofa"))
-                      :vars '((user-mail-address . "padillaj@ualberta.ca")
-                              (user-full-name . "Alex Jackson")
-                              (mu4e-sent-folder . "/uofa/[Gmail].Sent Mail")
-                              (mu4e-drafts-folder . "/uofa/[Gmail].Drafts")
-                              (mu4e-trash-folder . "/uofa/[Gmail].Trash")
-                              (mu4e-refile-folder . "/uofa/[Gmail].All Mail")
-                              (mu4e-sent-messages-behaviour . 'delete)))
-                    ,(make-mu4e-context
                       :name "Geography AS"
                       :match-func (lambda (msg) (alex/mu4e-match-account msg "geography"))
                       :vars '((user-mail-address . "alex@geographyas.info")
@@ -63,7 +53,7 @@
                               (mu4e-sent-messsages-behaviour . 'delete)))))
 
             (add-to-list 'mu4e-bookmarks
-                         '("maildir:/personal/INBOX OR maildir:/uofa/INBOX OR maildir:/geography/INBOX OR maildir:/leeds/INBOX" "Unified Inbox" ?i))
+                         '("maildir:/personal/INBOX OR maildir:/geography/INBOX" "Unified Inbox" ?i))
 
             ;; Prompt to sign messages with PGP key
             (add-hook 'message-send-hook
