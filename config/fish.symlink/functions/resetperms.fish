2ba3d46973fcec5df88b817e1fe61291b84582de
Alex Jackson <alex@alexj.org>
2017-09-29 15:12:09 +0100

    Modify resetperms function to accept target directories as arguments

--- config/fish.symlink/functions/resetperms.fish
+++ config/fish.symlink/functions/resetperms.fish
@@ -1,4 +1,10 @@
-function resetperms -d "Recursively sets directory permissions to 044 and file permissions to 0755 in the current directory"
-  find . -type f -exec chmod 0644 '{}' \;
-  find . -type d -exec chmod 0755 '{}' \;
+function resetperms -d "Recursively sets directory permissions to 0644 and file permissions to 0755"
+  if [ (count $argv) -lt 1 ]
+    set TARGET (pwd)
+  else
+    set TARGET $argv
+  end
+
+  find "$TARGET" -type f -exec chmod 0644 '{}' \;
+  find "$TARGET" -type d -exec chmod 0755 '{}' \;
 end
