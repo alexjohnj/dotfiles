8b435b0a66f180eafdbafe705b644b26f14e54a2
Alex Jackson <alex@alexj.org>
2018-04-15 18:20:01 +0100

    Define shortcuts for ivy-dispatching-done and ivy-immediate-done

--- emacs/emacs.d.symlink/init.el
+++ emacs/emacs.d.symlink/init.el
@@ -112,6 +112,8 @@
             (setq ivy-use-virtual-buffers t
                   projectile-completion-system 'ivy)
             (with-eval-after-load "ivy"
+              (define-key ivy-minibuffer-map (kbd "<C-return>") 'ivy-dispatching-done)
+              (define-key ivy-minibuffer-map (kbd "<C-M-return>") 'ivy-immediate-done)
               (define-key ivy-minibuffer-map (kbd "C-j") 'ivy-next-line)
               (define-key ivy-minibuffer-map (kbd "C-k") 'ivy-previous-line)
               (define-key ivy-minibuffer-map [escape] (kbd "C-g")))
