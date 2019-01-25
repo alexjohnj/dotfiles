e23fd4a9f20cdb32362473b27edf5cb87df7de1b
Alex Jackson <alex@alexj.org>
2019-01-25 17:15:18 +0000

    Enable rainbow-delimiters in all prog-mode derivatives

--- emacs/emacs.d.symlink/package-config/init-rainbow-delimiters.el
+++ emacs/emacs.d.symlink/package-config/init-rainbow-delimiters.el
@@ -1,8 +1,5 @@
 (use-package rainbow-delimiters
   :ensure t
-  :commands (rainbow-delimiters-mode)
-  :init (progn
-          (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
-          (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)))
+  :hook (prog-mode-hook . rainbow-delimiters-mode))
 
 (provide 'init-rainbow-delimiters)
