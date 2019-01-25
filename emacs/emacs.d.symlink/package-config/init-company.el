e5b75e19a3bb49dd5f17e5c4015d8e96e9fe5c6a
Alex Jackson <alex@alexj.org>
2019-01-25 17:15:51 +0000

    Update company config to allow non-matching input

--- emacs/emacs.d.symlink/package-config/init-company.el
+++ emacs/emacs.d.symlink/package-config/init-company.el
@@ -1,15 +1,14 @@
 (use-package company
   :ensure t
+  :hook (after-init . global-company-mode)
   :diminish company-mode
-  :commands (global-company-mode)
-  :init (progn (add-hook 'after-init-hook 'global-company-mode))
-  :config (progn
-            (setq company-idle-delay 0.3
-                       company-minimum-prefix-length 2
-                       company-show-numbers t
-                       company-dabbrev-downcase nil
-                       company-dabbrev-ignore-case t
-                       company-dabbrev-code-ignore-case t
-                       company-dabbrev-code-everywhere t)))
+  :config (setq company-idle-delay 0.3
+                company-minimum-prefix-length 2
+                company-show-numbers t ;; Show numbers next to each completion option
+                company-require-match nil ;; Allow non-matching input
+                company-dabbrev-downcase nil ;; Don't downcase completions
+                company-dabbrev-ignore-case t ;; Ignore case when completing
+                company-dabbrev-code-ignore-case t
+                company-dabbrev-code-everywhere t))
 
 (provide 'init-company)
