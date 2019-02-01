02d527b6e5bcab2f2a3a25e8023877279844833f
Alex Jackson <alex@alexj.org>
2019-02-01 16:21:28 +0000

    Configure paredit hook using use-package

--- emacs/emacs.d.symlink/package-config/init-paredit.el
+++ emacs/emacs.d.symlink/package-config/init-paredit.el
@@ -1,9 +1,6 @@
 (use-package paredit
   :ensure t
-  :commands (enable-paredit-mode)
-  :diminish paredit-mode
-  :init (progn
-          (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
-          (add-hook 'clojure-mode-hook 'enable-paredit-mode)))
+  :diminish  paredit-mode
+  :hook (emacs-lisp-mode . enable-paredit-mode))
 
 (provide 'init-paredit)
