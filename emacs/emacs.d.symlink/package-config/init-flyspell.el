92da7c46f49ac395bbc3c5c928cfe75858b0a4e3
Alex Jackson <alex@alexj.org>
2019-02-01 16:22:56 +0000

    Configure flyspell hooks using use-package

--- emacs/emacs.d.symlink/package-config/init-flyspell.el
+++ emacs/emacs.d.symlink/package-config/init-flyspell.el
@@ -1,12 +1,11 @@
 (use-package flyspell
   :ensure t
   :diminish flyspell-mode
-  :commands (flyspell-prog-mode flyspell-mode)
+  :hook ((prog-mode . flyspell-prog-mode)
+         (text-mode . flyspell-mode))
   :init (progn
           (setq ispell-program-name "aspell"
                 ispell-dictionary "en_GB")
-          (add-hook 'prog-mode-hook 'flyspell-prog-mode)
-          (add-hook 'text-mode-hook 'flyspell-mode)
           (evil-leader/set-key
             "S t" 'flyspell-mode
             "S s" 'ispell-buffer
