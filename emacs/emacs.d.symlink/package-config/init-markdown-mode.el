44e0024a2ffa62c0130ff75ef3c4c44ba002eef7
Alex Jackson <alex@alexj.org>
2019-02-01 16:26:06 +0000

    Configure flyspell-mode hook in markdown-mode

--- emacs/emacs.d.symlink/package-config/init-markdown-mode.el
+++ emacs/emacs.d.symlink/package-config/init-markdown-mode.el
@@ -2,6 +2,7 @@
   :ensure t
   :mode (("\\.markdown\\'" . gfm-mode)
          ("\\.md\\'" . gfm-mode))
+  :hook (markdown-mode . flyspell-mode)
   :init (progn
           (setq markdown-italic-underscore t
                 markdown-enable-math t))
@@ -9,10 +10,3 @@
             (add-hook 'markdown-mode-hook 'flyspell-mode)))
 
 (provide 'init-markdown-mode)
-
-
-
-
-
-
-
