ddefdb96ad5c8fdd4617d7a5a31626df3f7d0b9d
Alex Jackson <alex@alexj.org>
2019-02-03 10:03:11 +0000

    Clean up fish aliases
    Replaced most aliases with abbreviations. Removed some unused aliases for matlab
    and julia.

--- config/fish.symlink/abbreviations.fish
+++ config/fish.symlink/abbreviations.fish
@@ -1,6 +1,5 @@
-# Enable colourised output in ls. This tries to be compatible with GNU
-# ls, BSD ls w/ colour support and BSD ls w/o colour support (e.g.,
-# OpenBSD).
+# Enable colourised output in ls. This tries to be compatible with GNU ls, BSD
+# ls w/ colour support and BSD ls w/o colour support (e.g., OpenBSD).
 if ls --color=always > /dev/null ^&1 # GNU
   alias ls "ls --color=always"
 else if ls -G > /dev/null ^&1 # BSD
@@ -13,29 +12,19 @@ end
 # happen someday.
 abbr rm " rm"
 
+# Ruby abbreviations
 abbr bex "bundle exec"
 abbr gem-nuke "gem uninstall -aIx" # Uninstalls all gems
 
 # Copy files with progress information
-alias cpv "rsync -avP"
+abbr cpv "rsync -avP"
 
 # Mirror two directories
-alias rmirror "rsync -ah --delete --progress"
-
-# MATLAB Aliases
-alias matlab "matlab -nodesktop"
-
-abbr julia "julia -q"
+abbr rmirror "rsync -ah --delete --progress"
 
 # Edit with Emacs
-alias e "emacsclient -n -a \"\""
-alias et "emacsclient -nw -a \"\""
-
-# Work with encrypted ledger files
-alias eledger "gpg --batch -d -q $LEDGER_FILE | ledger -f - "
-alias ehledger "gpg --batch -d -q $LEDGER_FILE | hledger -f -"
-alias done "osascript -e 'display notification with title \"Done\"'"
-abbr ledger "ledger --strict"
+abbr e "emacsclient -n -a \"\""
+abbr et "emacsclient -nw -a \"\""
 
 # Aliases for Beets
 abbr beet-flac2alac "beet convert -k -f alac -d . format:FLAC"
