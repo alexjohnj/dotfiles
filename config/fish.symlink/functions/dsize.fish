087aea9692cdb6a85116d8eb9c93c84eab1ed99b
Alex Jackson <alex@alexj.org>
2019-02-16 10:03:14 +0000

    Remove dependency on gsort from dsize function

--- config/fish.symlink/functions/dsize.fish
+++ config/fish.symlink/functions/dsize.fish
@@ -1,9 +1,9 @@
 # Shortcut for the `du` command with human readable output, sorting
 # and a grand total when used with multiple files.
-function dsize -d "A shortcut for du -csh | gsort -rh"
+function dsize -d "A shortcut for du -csh | sort -rh"
   if [ (count $argv) -le 1 ]
-    du -sh $argv | gsort -rh
+    du -sh $argv | sort -rh
   else
-    du -csh $argv | gsort -rh
+    du -csh $argv | sort -rh
   end
 end
