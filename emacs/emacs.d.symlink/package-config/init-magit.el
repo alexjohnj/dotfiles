981f2f36277fa2f76caaaf65eea54f8537139784
Alex Jackson <alex@alexj.org>
2019-05-24 14:27:31 +0100

    Change magit log shortcut to run magit-log-all-branches

--- emacs/emacs.d.symlink/package-config/init-magit.el
+++ emacs/emacs.d.symlink/package-config/init-magit.el
@@ -9,7 +9,7 @@
             "g s" 'magit-status
             "g S" 'magit-stage-file
             "g C" 'magit-commit-create
-            "g l" 'magit-log-all
+            "g l" 'magit-log-all-branches
             "g L" 'magit-log-buffer-file)
           (which-key-add-key-based-replacements "SPC g" "Magit"))
   :config (progn
