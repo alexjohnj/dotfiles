a42ed411dd07e19518a0954364780f4fbee5ae4e
Alex Jackson <alex@alexj.org>
2019-10-06 10:32:33 +0100

    Tweak global magit keybindings
    Typing "leader-g" will bring up the magit dispatching popup. Pressing "s" will
    then bring up the magit status window, otherwise I can run any git command from
    any buffer.
    
    In theory, this is awesome.

--- emacs/emacs.d.symlink/package-config/init-magit.el
+++ emacs/emacs.d.symlink/package-config/init-magit.el
@@ -1,19 +1,16 @@
 (use-package magit
   :ensure t
-  :commands (magit-status
-             magit-log-other
-             magit-commit-create
-             magit-stage-file)
+  :commands (magit-dispatch-popup magit-status)
   :init (progn
-          (evil-leader/set-key
-            "g s" 'magit-status
-            "g S" 'magit-stage-file
-            "g C" 'magit-commit-create
-            "g l" 'magit-log-all-branches
-            "g L" 'magit-log-buffer-file)
+          (evil-leader/set-key "g" 'magit-dispatch-popup)
           (which-key-add-key-based-replacements "SPC g" "Magit"))
   :config (progn
-            (setq magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1)))
+            (define-key magit-popup-mode-map (kbd "s")
+              '(lambda ()
+                 (interactive)
+                 (magit-popup-quit)
+                 (magit-status)))
+            (setq magit-display-buffer-function 'magit-display-buffer-fullcolumn-most-v1)))
 
 (use-package evil-magit
   :ensure t
