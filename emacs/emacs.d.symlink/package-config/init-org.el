711e491cecfc2c9328a6ed01c7ca58ff31957823
Alex Jackson <alex@alexj.org>
2020-03-31 17:26:50 +0100

    Update org-mode use-package statement to fit new style

--- emacs/emacs.d.symlink/package-config/init-org.el
+++ emacs/emacs.d.symlink/package-config/init-org.el
@@ -3,30 +3,29 @@
 ;;; Code:
 
 (use-package org
-  :mode ("\\.org\\'" . org-mode)
-  :config (progn
-            (setq org-startup-indented t
-                  org-list-indent-offset 1
-                  org-goto-interface 'outline-path-completion
-                  org-goto-max-level 10
-                  org-outline-path-complete-in-steps nil
-                  org-catch-invisible-edits 'show-and-error
-                  org-cycle-separator-lines 0)
+  :mode (("\\.org\\'" . org-mode))
+  :config
+  (setq org-startup-indented t
+        org-list-indent-offset 1
+        org-goto-interface 'outline-path-completion
+        org-goto-max-level 10
+        org-outline-path-complete-in-steps nil
+        org-catch-invisible-edits 'show-and-error
+        org-cycle-separator-lines 0)
 
-            (add-hook 'org-mode-hook 'auto-fill-mode)
-            (add-hook 'org-mode-hook (lambda ()
-                                       (define-key evil-normal-state-map (kbd "TAB") 'org-cycle)))
+  (add-hook 'org-mode-hook 'auto-fill-mode)
+  (add-hook 'org-mode-hook (lambda ()
+                             (define-key evil-normal-state-map (kbd "TAB") 'org-cycle)))
 
-            (evil-leader/set-key-for-mode 'org-mode
-              "m E" 'org-export-dispatch
-              "m p l" 'org-toggle-latex-fragment
-              "m p i" 'org-toggle-inline-images
-              "m p u" 'org-toggle-pretty-entities
-              "m n s" 'org-narrow-to-subtree
-              "m n b" 'org-narrow-to-block
-              "m n w" 'widen
-              "m n i" 'org-tree-to-indirect-buffer)))
-
+  (evil-leader/set-key-for-mode 'org-mode
+    "m E" 'org-export-dispatch
+    "m p l" 'org-toggle-latex-fragment
+    "m p i" 'org-toggle-inline-images
+    "m p u" 'org-toggle-pretty-entities
+    "m n s" 'org-narrow-to-subtree
+    "m n b" 'org-narrow-to-block
+    "m n w" 'widen
+    "m n i" 'org-tree-to-indirect-buffer))
 
 (provide 'init-org)
 ;;; init-org.el ends here
