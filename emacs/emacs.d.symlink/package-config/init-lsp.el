b28fa5afeb24bb0a416bbc8997e4001269544144
Alex Jackson <alex@alexj.org>
2020-04-07 19:16:31 +0100

    Add sourcekit-lsp package to Emacs

--- emacs/emacs.d.symlink/package-config/init-lsp.el
+++ emacs/emacs.d.symlink/package-config/init-lsp.el
@@ -1,7 +1,8 @@
 (use-package lsp-mode
   :commands lsp
   :hook ((python-mode . lsp)
-         (rust-mode . lsp)))
+         (rust-mode . lsp)
+         (swift-mode . lsp)))
 
 (use-package lsp-ui
   :after lsp-mode
@@ -9,5 +10,11 @@
 
 (use-package company-lsp
   :commands company-lsp)
+
+(use-package lsp-sourcekit
+  :after lsp-mode
+  :config
+  (setenv "SOURCEKIT_TOOLCHAIN_PATH" "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain")
+  (setq lsp-sourcekit-executable (expand-file-name "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp")))
 
 (provide 'init-lsp)
