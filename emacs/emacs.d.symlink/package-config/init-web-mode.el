b2f77935cd448a78d9606d3eb066bc2fae2a08dd
Alex Jackson <alex@alexj.org>
2020-05-07 17:40:26 +0100

    Improve web-mode configuration for editing eex templates

--- emacs/emacs.d.symlink/package-config/init-web-mode.el
+++ emacs/emacs.d.symlink/package-config/init-web-mode.el
@@ -1,11 +1,20 @@
 (use-package web-mode
-  :mode ("\\.html\\'" . web-mode)
-  :init (progn
-          (setq web-mode-markup-indent-offset 2
-                web-mode-css-indent-offset 2
-                web-mode-code-indent-offset 2
-                web-mode-enable-auto-pairing t
-                web-mode-enable-engine-detection t
-                web-mode-enable-current-element-highlight t)))
+  :mode (("\\.html\\'" . web-mode)
+         ("\\.html.eex\\'" . web-mode))
+  :config
+  (setq web-mode-markup-indent-offset 2
+        web-mode-css-indent-offset 2
+        web-mode-code-indent-offset 2
+        web-mode-enable-auto-pairing nil
+        web-mode-enable-engine-detection t
+        web-mode-enable-current-element-highlight t)
+
+  (add-hook 'web-mode-hook #'alex/web-mode--configure-eex-pairing))
 
 (provide 'init-web-mode)
+
+(defun alex/web-mode--configure-eex-pairing ()
+  (sp-with-modes '(web-mode)
+    (sp-local-pair "<% " " %>" :insert "C-b %")
+    (sp-local-pair "<%= " " %>" :insert "C-b =")
+    (sp-local-pair "<%# " " %>" :insert "C-b #")))
