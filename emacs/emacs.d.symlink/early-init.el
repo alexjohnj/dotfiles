15327c6bb16b9b8452951c71fc7413496176ee61
Alex Jackson <alex@alexj.org>
2020-05-08 10:20:58 +0100

    Steal doom-emacs' early-init config
    It's mostly the same as my one.

--- emacs/emacs.d.symlink/early-init.el
+++ emacs/emacs.d.symlink/early-init.el
@@ -1,11 +1,26 @@
 ;; -*- lexical-binding: t -*-
 
-;; Disable package system since I configure it myself
-(setq package-enable-at-startup nil)
+;; Most of this is taken from doom-emacs.
 
-;; Configure default frame properties
-(push '(width . 120) default-frame-alist)
-(push '(height . 80) default-frame-alist)
-(push '(tool-bar-lines . 0) default-frame-alist)
+;; Defer garbage collection further back in the startup process
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; In Emacs 27+, package initialization occurs before `user-init-file' is
+;; loaded, but after `early-init-file'. I'm using straight.el for package
+;; management so this isn't needed.
+(setq package-enable-at-startup nil)
+(advice-add #'package--ensure-init-file :override #'ignore)
+
+;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
 (push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
+
+;; Make the default frame a bit bigger.
+(push '(width . 140) default-frame-alist)
+(push '(height . 100) default-frame-alist)
+
+;; Resizing the Emacs frame can be a terribly expensive part of changing the
+;; font. By inhibiting this, we easily halve startup times with fonts that are
+;; larger than the system default.
+(setq frame-inhibit-implied-resize t)
