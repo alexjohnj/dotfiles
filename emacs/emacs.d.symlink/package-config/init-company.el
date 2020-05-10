dcdaf6fca07467f48c961f42cbb2e7d8c71a7195
Alex Jackson <alex@alexj.org>
2020-05-10 15:32:28 +0100

    Set minimum prefix for company-mode completion

--- emacs/emacs.d.symlink/package-config/init-company.el
+++ emacs/emacs.d.symlink/package-config/init-company.el
@@ -2,13 +2,12 @@
   :hook (after-init . global-company-mode)
   :diminish company-mode
   :config (setq company-idle-delay 0
-                company-minimum-prefix-length 0
+                company-minimum-prefix-length 1
                 company-tooltip-idle-delay 0
                 company-show-numbers t ;; Show numbers next to each completion option
-                company-require-match nil ;; Allow non-matching input
+                company-require-match 'never ;; Allow non-matching input
+                company-dabbrev-other-buffers nil ;; Don't use other buffers as a completion source
                 company-dabbrev-downcase nil ;; Don't downcase completions
-                company-dabbrev-ignore-case t ;; Ignore case when completing
-                company-dabbrev-code-ignore-case t
-                company-dabbrev-code-everywhere t))
+                company-dabbrev-ignore-case nil))
 
 (provide 'init-company)
