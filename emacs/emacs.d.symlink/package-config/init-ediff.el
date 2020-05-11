439a1a8bb685dfe2266c97e14d88f1d1984c5dc1
Alex Jackson <alex@alexj.org>
2020-05-11 09:45:55 +0100

    Defer loading ediff package

--- emacs/emacs.d.symlink/package-config/init-ediff.el
+++ emacs/emacs.d.symlink/package-config/init-ediff.el
@@ -1,16 +1,17 @@
 (use-package ediff
-  :config (progn
-            (setq ediff-window-setup-function 'ediff-setup-windows-plain
-                  ediff-split-window-function 'split-window-horizontally)
+  :commands (ediff)
+  :config
+  (setq ediff-window-setup-function 'ediff-setup-windows-plain
+        ediff-split-window-function 'split-window-horizontally)
 
-            (defun ediff-copy-both-to-C ()
-              "Merge the diff in buffer A and buffer B into C"
-              (interactive)
-              (ediff-copy-diff ediff-current-difference nil 'C nil
-                               (concat
-                                (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
-                                (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
-            (defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
-            (add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map)))
+  (defun ediff-copy-both-to-C ()
+    "Merge the diff in buffer A and buffer B into C"
+    (interactive)
+    (ediff-copy-diff ediff-current-difference nil 'C nil
+                     (concat
+                      (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
+                      (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
+  (defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
+  (add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map))
 
 (provide 'init-ediff)
