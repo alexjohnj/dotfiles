9186d4a3dc80e645d1acb2fe236c3616e531ea04
Alex Jackson <alex@alexj.org>
2020-10-24 14:02:41 +0100

    Disable evil key bindings in deft-mode

--- emacs/emacs.d.symlink/package-config/init-deft.el
+++ emacs/emacs.d.symlink/package-config/init-deft.el
@@ -1,11 +1,11 @@
 (use-package deft
   :commands (deft)
-  :init (progn
-          (evil-leader/set-key "n" 'deft))
-  :config (progn
-            (setq deft-directory "~/Documents/Notes/"
-                  deft-default-extension "org"
-                  deft-use-filter-string-for-filename t
-                  deft-org-mode-title-prefix t)))
+  :init (evil-leader/set-key "n" #'deft)
+  :config
+  (add-to-list 'evil-emacs-state-modes 'deft-mode)
+  (setq deft-directory "~/Documents/Notes/"
+        deft-default-extension "org"
+        deft-use-filter-string-for-filename t
+        deft-org-mode-title-prefix t))
 
 (provide 'init-deft)
