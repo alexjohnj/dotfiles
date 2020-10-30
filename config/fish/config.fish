bd9b042fb781040d431723db6a7b1b979388cfbf
Alex Jackson <alex@alexj.org>
2020-10-30 08:56:41 +0000

    Add asdf to fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,2 +1,8 @@
-. "$HOME/.config/fish/exports.fish" # Load Exports
-. "$HOME/.config/fish/abbreviations.fish" # Load Aliases
+source "$HOME/.config/fish/exports.fish" # Load Exports
+source "$HOME/.config/fish/abbreviations.fish" # Load Aliases
+
+
+set -l ASDF_CONFIG_FILE "/usr/local/opt/asdf/asdf.fish"
+if test -f $ASDF_CONFIG_FILE
+    source $ASDF_CONFIG_FILE
+end
