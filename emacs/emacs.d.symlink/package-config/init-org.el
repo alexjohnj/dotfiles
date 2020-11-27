85763ea4198fbe132844a45db1b1f4caaf6a0359
Alex Jackson <alex@alexj.org>
2020-11-27 09:32:02 +0000

    Add evil-leader bindings for org-clock

--- emacs/emacs.d.symlink/package-config/init-org.el
+++ emacs/emacs.d.symlink/package-config/init-org.el
@@ -25,7 +25,13 @@
     "m n s" 'org-narrow-to-subtree
     "m n b" 'org-narrow-to-block
     "m n w" 'widen
-    "m n i" 'org-tree-to-indirect-buffer))
+    "m n i" 'org-tree-to-indirect-buffer)
+
+  (evil-leader/set-key-for-mode 'org-mode
+    "m c i" 'org-clock-in
+    "m c o" 'org-clock-out
+    "m c C" 'org-clock-cancel
+    "m c g" 'org-clock-goto))
 
 (provide 'init-org)
 ;;; init-org.el ends here
