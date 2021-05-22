50f9073880b2f33f1bf4917e66ce598477465954
Alex Jackson <alex@alexj.org>
2021-05-22 13:05:22 +0100

    Fontify code blocks in markdown-mode

--- emacs/emacs.d.symlink/package-config/init-markdown-mode.el
+++ emacs/emacs.d.symlink/package-config/init-markdown-mode.el
@@ -2,10 +2,11 @@
   :mode (("\\.markdown\\'" . gfm-mode)
          ("\\.md\\'" . gfm-mode))
   :hook (markdown-mode . flyspell-mode)
-  :init (progn
-          (setq markdown-italic-underscore t
-                markdown-enable-math t))
-  :config (progn
-            (add-hook 'markdown-mode-hook 'flyspell-mode)))
+  :init
+  (setq markdown-italic-underscore t
+        markdown-fontify-code-blocks-natively t
+        markdown-enable-math t)
+  :config
+  (add-hook 'markdown-mode-hook 'flyspell-mode))
 
 (provide 'init-markdown-mode)
