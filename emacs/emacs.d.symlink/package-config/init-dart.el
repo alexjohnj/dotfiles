5338b9bce07bda7a9bb9743ca56108cbf0184fd3
Alex Jackson <alex@alexj.org>
2021-06-05 09:44:12 +0100

    Add basic support for dart/flutter installations using asdf in Emacs

--- emacs/emacs.d.symlink/package-config/init-dart.el
+++ emacs/emacs.d.symlink/package-config/init-dart.el
@@ -6,6 +6,13 @@
     (add-to-list 'projectile-project-root-files-bottom-up "pubspec.yaml")))
 
 (use-package lsp-dart
-  :after (dart-mode))
+  :after (dart-mode)
+  :config
+  ;; TODO: How to tie this to the flutter version for a project?
+  (setq lsp-dart-flutter-sdk-dir (expand-file-name "~/.asdf/installs/flutter/1.22.4")
+        lsp-dart-dap-flutter-hot-reload-on-save t)
+  (evil-leader/set-key-for-mode 'dart-mode
+    "m r" #'lsp-dart-dap-flutter-hot-reload
+    "m R" #'lsp-dart-dap--flutter-hot-restart))
 
 (provide 'init-dart)
