8b34e48baeb10721ee9a4d2eb8ed980d43b399d3
Alex Jackson <alex@alexj.org>
2021-06-07 10:33:24 +0100

    Fix broken custom ediff bindings

--- emacs/emacs.d.symlink/package-config/init-ediff.el
+++ emacs/emacs.d.symlink/package-config/init-ediff.el
@@ -4,7 +4,7 @@
   (setq ediff-window-setup-function 'ediff-setup-windows-plain
         ediff-split-window-function 'split-window-horizontally)
 
-  (defun ediff-copy-both-to-C ()
+  (defun alex/ediff-copy-both-to-C ()
     "Merge the diff in buffer A and buffer B into C"
     (interactive)
     (ediff-copy-diff ediff-current-difference nil 'C nil
@@ -12,7 +12,10 @@
                       (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
                       (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
 
-  (general-def ediff-mode-map
-    "D" #'ediff-copy-both-to-C))
+  ;; ediff creates a new keymap each time it is started so custom bindings must
+  ;; be set in a hook.
+  (add-hook 'ediff-keymap-setup-hook
+            #'(lambda ()
+                (general-def ediff-mode-map "D" #'alex/ediff-copy-both-to-C))))
 
 (provide 'init-ediff)
