ecd31ffd59ddf200546990f08f876f44f01b2f62
Alex Jackson <alex@alexj.org>
2021-06-18 16:39:33 +0100

    Set default visibility of sections in magit

--- emacs/emacs.d.symlink/package-config/init-magit.el
+++ emacs/emacs.d.symlink/package-config/init-magit.el
@@ -10,6 +10,10 @@
   :init
   (which-key-add-key-based-replacements "SPC g" "Magit")
   :config
+  (setq magit-section-initial-visibility-alist
+        '(([unpushed status] . show)
+          ([unstaged status] . show)
+          ([untracked status] . show)))
   (setq magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1))
 
 (provide 'init-magit)
