c71eb0d3b359b7ab644cd2c7469d6d64bb5f2765
Alex Jackson <alex@alexj.org>
2022-02-12 14:33:30 +0000

    Delete more things in the xcfix script

--- config/fish/functions/xcfix.fish
+++ config/fish/functions/xcfix.fish
@@ -1,9 +1,9 @@
 function xcfix -d "Fix Xcode by nuking DerivedData"
     set -l DERIVED_DATA "$HOME/Library/Developer/Xcode/DerivedData"
-
-    if not test -d $DERIVED_DATA
-        return 0
-    end
+    set -l CACHE_DIR "$HOME/Library/Caches/com.apple.dt.Xcode"
+    set -l CLANG_MODULE_CACHE_DIR "$TMPDIR/../C/clang/ModuleCache".
 
     rm -rf $DERIVED_DATA
+    rm -rf $CACHE_DIR
+    rm -rf $CLANG_MODULE_CACHE_DIR
 end
