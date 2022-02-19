ce4f41893c1814ffe858d45bdd7af642e7871791
Alex Jackson <alex@alexj.org>
2022-02-19 09:25:47 +0000

    Only source asdf config when brew command is available

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,6 @@
 source "$HOME/.config/fish/exports.fish" # Load Exports
 source "$HOME/.config/fish/abbreviations.fish" # Load Aliases
-source (brew --prefix asdf)/libexec/asdf.fish
+
+if type -q "brew"
+    source (brew --prefix asdf)/libexec/asdf.fish
+end
