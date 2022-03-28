b80c333ac16afd9af8bf085a77233e2b276b0924
Alex Jackson <alex@alexj.org>
2022-03-28 08:59:31 +0100

    Delete Xcode application state in xcfix function

--- config/fish/functions/xcfix.fish
+++ config/fish/functions/xcfix.fish
@@ -2,8 +2,10 @@ function xcfix -d "Fix Xcode by nuking DerivedData"
     set -l DERIVED_DATA "$HOME/Library/Developer/Xcode/DerivedData"
     set -l CACHE_DIR "$HOME/Library/Caches/com.apple.dt.Xcode"
     set -l CLANG_MODULE_CACHE_DIR "$TMPDIR/../C/clang/ModuleCache".
+    set -l APPLICATION_STATE "$HOME/Library/Saved Application State/com.apple.dt.Xcode.savedState"
 
     rm -rf $DERIVED_DATA
     rm -rf $CACHE_DIR
     rm -rf $CLANG_MODULE_CACHE_DIR
+    rm -rf $APPLICATION_STATE
 end
