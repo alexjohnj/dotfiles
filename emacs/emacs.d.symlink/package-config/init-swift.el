e9b35ff3cfed296b3499d6c44542ceef4bdb16d4
Alex Jackson <alex@alexj.org>
2022-10-14 09:08:01 +0100

    Set fill-column to 120 in swift-mode

--- emacs/emacs.d.symlink/package-config/init-swift.el
+++ emacs/emacs.d.symlink/package-config/init-swift.el
@@ -1,10 +1,14 @@
 (use-package swift-mode
   :mode ("\\.swift\\'" . swift-mode)
-  :hook ((swift-mode . tree-sitter-hl-mode)))
+  :hook ((swift-mode . tree-sitter-hl-mode)
+         (swift-mode . alex/swift-mode-hook)))
 
 (use-package flycheck-swift
   :after (flycheck swift-mode)
   :config (progn
             (add-to-list 'flycheck-checkers 'swift)))
+
+(defun alex/swift-mode-hook ()
+  (setq fill-column 120))
 
 (provide 'init-swift)
