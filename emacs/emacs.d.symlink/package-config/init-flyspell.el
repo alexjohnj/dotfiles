44275ad29ba5b3f76a60d6e0a5cf6ff7edde32b8
Alex Jackson <alex@alexj.org>
2022-10-29 10:34:01 +0100

    Update flyspell use-package config

--- emacs/emacs.d.symlink/package-config/init-flyspell.el
+++ emacs/emacs.d.symlink/package-config/init-flyspell.el
@@ -2,16 +2,16 @@
   :diminish flyspell-mode
   :hook ((prog-mode . flyspell-prog-mode)
          (text-mode . flyspell-mode))
-  :init (progn
-          (which-key-add-key-based-replacements "SPC S" "Spelling"))
-  :config (progn
-            (when (executable-find "aspell")
-              (setq ispell-program-name "aspell"
-                    ispell-dictionary "en_GB"
-                    ispell-extra-args '("--sug-mode=ultra")))
-            (alex/leader-def
-              "S t" #'flyspell-mode
-              "S s" #'ispell-buffer
-              "S S" #'flyspell-buffer)))
+  :init
+  (which-key-add-key-based-replacements "SPC S" "Spelling")
+  :general (alex/leader-def
+             "S t" #'flyspell-mode
+             "S s" #'ispell-buffer
+             "S S" #'flyspell-buffer)
+  :config
+  (when (executable-find "aspell")
+    (setq ispell-program-name "aspell"
+          ispell-dictionary "en_GB"
+          ispell-extra-args '("--sug-mode=ultra"))))
 
 (provide 'init-flyspell)
