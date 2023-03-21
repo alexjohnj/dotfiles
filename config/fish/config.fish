c533ce6ad9afc7ac1979d7c7b94157afb5978024
Alex Jackson <alex@alexj.org>
2023-03-21 09:11:33 +0000

    Only source asdf config if asdf is installed

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,6 @@
 source "$HOME/.config/fish/exports.fish" # Load Exports
 source "$HOME/.config/fish/abbreviations.fish" # Load Aliases
 
-if type -q "brew"
+if type -q "brew"; and test -f (brew --prefix asdf)/libexec/asdf.fish
     source (brew --prefix asdf)/libexec/asdf.fish
 end
