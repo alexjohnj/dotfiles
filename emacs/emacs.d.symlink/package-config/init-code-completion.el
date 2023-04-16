016ee34a48ecf5a4d6162905fc9f90067e03c6cc
Alex Jackson <alex@alexj.org>
2023-04-16 09:53:44 +0100

    Update Emacs packages for April 2023

--- emacs/emacs.d.symlink/package-config/init-code-completion.el
+++ emacs/emacs.d.symlink/package-config/init-code-completion.el
@@ -1,12 +1,16 @@
 ;; -*- lexical-binding: t; -*-
 
 (use-package corfu
-  :hook (after-init . global-corfu-mode)
+  :straight (:files (:defaults "extensions/*")) ;; Loads additional extensions from repo
+  :hook ((after-init . global-corfu-mode)
+         (global-corfu-mode . corfu-popupinfo-mode))
   :config
-  ;; TODO: How do I enable corfu-indexed-mode?
   (setq corfu-auto t
         corfu-auto-delay 0
-        corfu-auto-prefix 1))
+        corfu-auto-prefix 1)
+
+  ;; Wait 0.5s to show the popup and then 1s to update it.
+  (setq corfu-popupinfo-delay '(0.5 . 1)))
 
 ;; Adds icons to the margins of the Corfu completion popup.
 (use-package kind-icon
@@ -15,13 +19,6 @@
   (setq kind-icon-default-face 'corfu-default
         kind-icon-use-icons nil) ;; Use text based icons instead of SVGs.
   (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
-
-;; Adds a documentation popup next to corfu completion candidates
-(use-package corfu-doc
-  :after corfu
-  :hook (corfu-mode . corfu-doc-mode)
-  :config
-  (setq corfu-doc-delay 0.2))
 
 ;; Provides additional completion at point functions
 (use-package cape
