bf11d2c964290d0dac95686620342c6604eec27c
Alex Jackson <alex@alexj.org>
2023-05-06 13:46:01 +0100

    Support configuring asdf installed from git repo

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,5 +2,9 @@ source "$HOME/.config/fish/exports.fish" # Load Exports
 source "$HOME/.config/fish/abbreviations.fish" # Load Aliases
 
 if type -q "brew"; and test -f (brew --prefix asdf)/libexec/asdf.fish
+    # asdf is installed with Homebrew
     source (brew --prefix asdf)/libexec/asdf.fish
+else if test -f ~/.asdf/asdf.fish
+    # asdf is installed from the git repo.
+    source ~/.asdf/asdf.fish
 end
