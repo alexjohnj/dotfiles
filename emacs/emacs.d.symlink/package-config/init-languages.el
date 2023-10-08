d9492377ed8e36f189b030146da1a5a9714348f9
Alex Jackson <alex@alexj.org>
2023-10-08 16:36:46 +0100

    Add basic configuration for built-in tree sitter grammars

--- emacs/emacs.d.symlink/package-config/init-languages.el
+++ emacs/emacs.d.symlink/package-config/init-languages.el
@@ -1,5 +1,25 @@
 ;; init-languages -- Programming Language Configuration
 
+(setq treesit-language-source-alist
+      '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+        (css "https://github.com/tree-sitter/tree-sitter-css")
+        (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+        (dockerfile "https://github.com/camdencheek/tree-sitter-dockerfile")
+        (html "https://github.com/tree-sitter/tree-sitter-html")
+        (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+        (json "https://github.com/tree-sitter/tree-sitter-json")
+        (make "https://github.com/alemuller/tree-sitter-make")
+        (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+        (python "https://github.com/tree-sitter/tree-sitter-python")
+        (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+        (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+        (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
+
+(defun alex/install-all-treesit-language-grammars ()
+  "Install all language grammars in the variable treesit-language-source-alist"
+  (interactive)
+  (mapc #'treesit-install-language-grammar (mapcar #'car treesit-language-source-alist)))
+
 (require 'init-beancount-mode)
 
 (use-package dockerfile-mode)
