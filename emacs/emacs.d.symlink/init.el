22af11b13c37e269eedae70935ec568597fb9cab
Alex Jackson <alex@alexj.org>
2023-10-08 15:27:42 +0100

    Remove after! macro from Emacs config

--- emacs/emacs.d.symlink/init.el
+++ emacs/emacs.d.symlink/init.el
@@ -70,61 +70,6 @@
   (setq esup-depth 0))
 
 
-;;; Supporting functions/macros
-
-(require 'cl-lib)
-
-;; Stolen from doom-emacs
-(defmacro after! (package &rest body)
-  "Evaluate BODY after PACKAGE have loaded.
-
-PACKAGE is a symbol or list of them. These are package names, not modes,
-functions or variables. It can be:
-
-- An unquoted package symbol (the name of a package)
-    (after! helm BODY...)
-- An unquoted list of package symbols (i.e. BODY is evaluated once both magit
-  and git-gutter have loaded)
-    (after! (magit git-gutter) BODY...)
-- An unquoted, nested list of compound package lists, using any combination of
-  :or/:any and :and/:all
-    (after! (:or package-a package-b ...)  BODY...)
-    (after! (:and package-a package-b ...) BODY...)
-    (after! (:and package-a (:or package-b package-c) ...) BODY...)
-  Without :or/:any/:and/:all, :and/:all are implied.
-
-This is a wrapper around `eval-after-load' that:
-
-1. Suppresses warnings for disabled packages at compile-time
-2. Supports compound package statements (see below)
-3. Prevents eager expansion pulling in autoloaded macros all at once"
-  (declare (indent defun) (debug t))
-  (if (symbolp package)
-      (list (if (or (not (bound-and-true-p byte-compile-current-file))
-                    (require package nil 'noerror))
-                #'progn
-              #'with-no-warnings)
-            (let ((body (macroexp-progn body)))
-              `(if (featurep ',package)
-                   ,body
-                 ;; We intentionally avoid `with-eval-after-load' to prevent
-                 ;; eager macro expansion from pulling (or failing to pull) in
-                 ;; autoloaded macros/packages.
-                 (eval-after-load ',package ',body))))
-    (let ((p (car package)))
-      (cond ((not (keywordp p))
-             `(after! (:and ,@package) ,@body))
-            ((memq p '(:or :any))
-             (macroexp-progn
-              (cl-loop for next in (cdr package)
-                       collect `(after! ,next ,@body))))
-            ((memq p '(:and :all))
-             (dolist (next (cdr package))
-               (setq body `((after! ,next ,@body))))
-             (car body))))))
-
-
-
 ;;; Emacs Server/Daemon
 
 ;; Start a new Emacs server instance if one isn't running.
@@ -382,7 +327,9 @@ This is a wrapper around `eval-after-load' that:
   :hook (emacs-lisp-mode . aggressive-indent-mode))
 
 ;; Configure tramp
-(after! tramp
+(use-package tramp
+  :straight nil
+  :config
   (setq tramp-verbose 5)
   (add-to-list 'tramp-default-user-alist
                '("ssh" "munchlax\\.local\\'" "root")))
