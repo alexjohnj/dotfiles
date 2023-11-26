3fd23f0452e2b5a03b6425a0b8728979cce5a742
Alex Jackson <alex@alexj.org>
2023-11-26 12:59:53 +0000

    Delete some unused scripts from the bin directory

--- bin.symlink/fwmoshconfg.fish
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env fish
-
-set -l MOSH (which mosh-server)
-
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate off
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --add $MOSH
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --unblockapp $MOSH
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
