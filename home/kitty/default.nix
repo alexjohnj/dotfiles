e3708f00b5b5d64ec819d8dc4e5f6af4c46a57b7
Alex Jackson <alex@alexj.org>
2024-04-30 20:10:29 +0100

    Fiddle with kitty settings

--- home/kitty/default.nix
+++ home/kitty/default.nix
@@ -3,13 +3,16 @@
     enable = true;
     font.name = "Zed Mono Extended";
     font.size = 13.0;
-    settings = {
+    settings = { # See https://sw.kovidgoyal.net/kitty/conf/
       confirm_os_window_close = 0;
       update_check_interval = 0;
       window_padding_width = "5 10";
+      cursor_blink_interval = 0;
+      tab_bar_style = "powerline";
+      tab_powerline_style = "round";
     };
     # Theme names ares specified in github.com/kovidgoyal/kitty-themes
     # Theme previews can be found in github.com/dexpota/kitty-themes
-    theme = "Tomorrow Night Eighties";
+    theme = "Modus Vivendi";
   };
 }
