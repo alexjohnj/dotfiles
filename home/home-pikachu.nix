53f254aa081811ea1c37fa99693e75f10c75008f
Alex Jackson <alex@alexj.org>
2024-08-23 18:58:56 +0100

    nix: Package prusa-slicer AppImage

--- home/home-pikachu.nix
+++ home/home-pikachu.nix
@@ -8,8 +8,9 @@
       f3d
       overpass
       inter
-      (callPackage ../packages/super-slicer/default.nix { })
       (callPackage ../packages/orca-slicer/default.nix { })
+      (callPackage ../packages/prusa-slicer/default.nix { })
+      (callPackage ../packages/super-slicer/default.nix { })
     ]
     ++ (with pkgs.gnomeExtensions; [
       dash-to-dock
