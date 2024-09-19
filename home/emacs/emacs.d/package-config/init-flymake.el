460b2dc4c1bd01242e0608e68477823e4bcfa0cd
Alex Jackson <alex@alexj.org>
2024-09-19 19:09:18 +0100

    emacs: Add flymake-eslint package

--- home/emacs/emacs.d/package-config/init-flymake.el
+++ home/emacs/emacs.d/package-config/init-flymake.el
@@ -11,4 +11,20 @@
   :init
   (which-key-add-key-based-replacements "SPC e" "Flymake"))
 
+(defun alex/flymake-eslint-enable ()
+  (when (and (or (derived-mode-p 'js-base-mode)
+                 (derived-mode-p 'typescript-ts-base-mode))
+             (executable-find "eslint"))
+    (flymake-eslint-enable)))
+
+(use-package flymake-eslint
+  :after (flymake)
+  :straight '(flymake-eslint :type git :host github :repo "orzechowskid/flymake-eslint")
+  :commands (flymake-eslint-enable)
+  ;; eglot will override flymake checkers when it starts so start the eslint checker
+  ;; after eglot with a custom hook. See: https://github.com/orzechowskid/flymake-eslint/issues/23
+  :hook (eglot-managed-mode . alex/flymake-eslint-enable)
+  :config
+  (setopt flymake-eslint-prefer-json-diagnostics t))
+
 (provide 'init-flymake)
