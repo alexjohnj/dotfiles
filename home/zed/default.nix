24cb6276a70b635c4c971acc5afe888c4ac50f84
Alex Jackson <alex@alexj.org>
2025-01-17 18:51:17 +0000

    zed: Use out of store symlink for config

--- home/zed/default.nix
+++ home/zed/default.nix
@@ -1,11 +1,15 @@
 {
   pkgs,
+  config,
   ...
 }:
+let
+  zedHome = "${config.home.homeDirectory}/dotfiles/home/zed";
+in
 {
   programs.zed-editor.enable = pkgs.stdenv.hostPlatform.isLinux;
   xdg.configFile = {
-    "zed/keymap.json".source = ./keymap.json;
-    "zed/settings.json".source = ./settings.json;
+    "zed/keymap.json".source = config.lib.file.mkOutOfStoreSymlink "${zedHome}/keymap.json";
+    "zed/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${zedHome}/settings.json";
   };
 }
