7601a8659ff5b3ed3817167eb141a53564bc341b
Alex Jackson <alex@alexj.org>
2025-01-25 17:57:16 +0000

    fixup! all: Catppuccin all the things

--- home/rofi/default.nix
+++ home/rofi/default.nix
@@ -3,4 +3,7 @@
   home.packages = [ pkgs.rofi-wayland ];
   xdg.configFile."rofi/config.rasi".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/home/rofi/config.rasi";
+
+  xdg.configFile."rofi/catppuccin-mocha.rasi".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/home/rofi/catppuccin-mocha.rasi";
 }
