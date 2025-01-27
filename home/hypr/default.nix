124db2ade977b543ac2dfff881b15eecb240150e
Alex Jackson <alex@alexj.org>
2025-01-27 19:20:27 +0000

    Disable default hyprland wallpaper

--- home/hypr/default.nix
+++ home/hypr/default.nix
@@ -8,8 +8,7 @@
   xdg.configFile."hypr/hyprland.conf".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/home/hypr/hyprland.conf";
 
-  xdg.configFile."hypr/hyprlock.conf".source =
-    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/home/hypr/hyprlock.conf";
+  xdg.configFile."hypr/hyprlock.conf".source = ./hyprlock.conf;
 
   home.packages = with pkgs; [
     swww
