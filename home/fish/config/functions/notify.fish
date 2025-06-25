21563512ba2ec397d776041af98a5a7bcdcba2ba
Alex Jackson <alex@alexj.org>
2025-06-25 16:41:56 +0100

    fish: Enhance notify function

--- home/fish/config/functions/notify.fish
+++ home/fish/config/functions/notify.fish
@@ -1,6 +1,18 @@
-function notify -d "Display a system notification"
-    set -l description "A command has finished running."
-    set -l title "Command Finished"
+function notify -d "Display a system notification" -a "command_name"
+    set -l og_status $status
+    if test -z $command_name
+        set command_name "A command"
+    end
+
+    if test $og_status -eq 0
+        set -f title "Command Succeeded"
+        set -f description "$command_name finished successfully"
+    else
+        set -f title "Command Failed"
+        set -f description "$command_name failed with status code $og_status"
+    end
 
     osascript -e "display notification \"$description\" with title \"$title\""
+
+    return $og_status
 end
