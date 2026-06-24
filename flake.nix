dc04058fd170f51140082c0f34a734c5eea9c7ae
Alex Jackson <alex@alexj.org>
2026-06-24 08:58:46 +0100

    nix: switch to nixpkgs-weekly input

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/0.1";
 
     home-manager = {
       url = "github:nix-community/home-manager";
